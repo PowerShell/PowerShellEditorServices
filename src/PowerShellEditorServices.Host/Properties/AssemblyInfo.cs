4308002f79308800ef6712ac3f54c5540d32115f
David Wilson <daviwil@microsoft.com>
2015-07-20 20:10:43 -0700

    Add .nuspec files for all shippable projects
    This change introduces .nuspec files so that NuGet packages can be
    built for all of the shippable projects in the solution.  Initial
    package build will be performed by AppVeyor and final package
    build for release will be done through some other means after the
    binaries have been signed and strong-named.

--- src/PowerShellEditorServices.Host/Properties/AssemblyInfo.cs
+++ src/PowerShellEditorServices.Host/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("PowerShellEditorServices.Host")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Windows PowerShell Editor Services - Host Process")]
+[assembly: AssemblyDescription("Provides a process for hosting the Windows PowerShell Editor Services library exposed by a corresponding transport library.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("PowerShellEditorServices.Host")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("Windows PowerShell Editor Services")]
+[assembly: AssemblyCopyright("© Microsoft Corporation. All rights reserved.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.0.0.0")]
+[assembly: AssemblyFileVersion("0.0.0.0")]
+[assembly: AssemblyInformationalVersion("0.0.0.0")]
--- src/PowerShellEditorServices.Transport.Stdio/Properties/AssemblyInfo.cs
+++ src/PowerShellEditorServices.Transport.Stdio/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("PowerShellEditorServices.Transport.Stdio")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Windows PowerShell Editor Services - Standard I/O Transport")]
+[assembly: AssemblyDescription("Provides standard I/O transport for the Windows PowerShell Editor Services host process.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("PowerShellEditorServices.Transport.Stdio")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("Windows PowerShell Editor Services")]
+[assembly: AssemblyCopyright("© Microsoft Corporation. All rights reserved.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.0.0.0")]
+[assembly: AssemblyFileVersion("0.0.0.0")]
+[assembly: AssemblyInformationalVersion("0.0.0.0")]
