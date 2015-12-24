8a98add6a68c638115d7dab176143f8371b62ba5
Keith Hill <r_keith_hill@hotmail.com>
2015-12-24 00:57:39 -0700

    Fix for issues #86 and #89.  Same fix for both where we need to unwrap PSObject when getting a variable's child variables.  Also added some more unit tests for variables.

--- test/PowerShellEditorServices.Test.Shared/Debugging/VariableTest.ps1
+++ test/PowerShellEditorServices.Test.Shared/Debugging/VariableTest.ps1
@@ -6,11 +6,15 @@
 function Test-Variables
 {
 	$strVar = "Hello"
-	$objVar = @{ firstChild = "Child"; secondChild = 42 }
 	$arrVar = @(1, 2, $strVar, $objVar)
+	$assocArrVar = @{ firstChild = "Child"; secondChild = 42 }
 	$classVar = [MyClass]::new();
 	$classVar.Name = "Test"
 	$classVar.Number = 42;
+    $enumVar = $ErrorActionPreference
+    $psObjVar = New-Object -TypeName PSObject -Property @{Name = 'John';  Age = 75}
+    $psCustomObjVar = [PSCustomObject] @{Name = 'Paul'; Age = 73}
+    $procVar = Get-Process system
 	Write-Output "Done"
 }
 
