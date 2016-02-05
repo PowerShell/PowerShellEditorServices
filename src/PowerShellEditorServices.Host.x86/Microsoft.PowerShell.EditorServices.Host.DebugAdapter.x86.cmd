2bdf4bf9f30472c3dc958dd4f7c3c1210704d3c3
David Wilson <daviwil@microsoft.com>
2016-02-04 19:17:35 -0800

    Remove debug adapter wrapper cmds for host process
    This change removes the
    Microsoft.PowerShell.EditorServices.DebugAdapter.cmd files which were used
    to wrap invocation of the host process to pass the /debugAdapter
    parameter.  This was needed because VS Code did not previously have a way
    to pass command line parameters to debug adapters at startup.
    
    Resolves #131

--- src/PowerShellEditorServices.Host.x86/Microsoft.PowerShell.EditorServices.Host.DebugAdapter.x86.cmd
+++
@@ -1,8 +0,0 @@
-@echo off
-
-REM This script is necessary until VS Code's debug adapter config allows command line parameters
-
-setlocal
-cd /d %~dp0
-
-Microsoft.PowerShell.EditorServices.Host.x86.exe /debugAdapter
