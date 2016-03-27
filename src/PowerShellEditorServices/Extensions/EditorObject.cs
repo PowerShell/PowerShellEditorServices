5f3a35e9fb2fbeca4f1985f67356ca9fffd50da5
David Wilson <daviwil@microsoft.com>
2016-03-27 14:50:18 -0700

    Introduce new editor extensibility API
    This change introduces a new PowerShell-based API that allows scripts and
    modules to extend the behavior of the host editor.  Scripts can either
    automate the editor directly or register commands that can later be
    invoked by the user.  The ExtensionService provides this behavior for any
    PowerShellContext by injecting a new variable '$psEditor' into the
    session.
    
    This version of the API is very early and will be expanded upon in future
    releases.

---
+++ src/PowerShellEditorServices/Extensions/EditorObject.cs
@@ -0,0 +1,86 @@
+//
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+//
+
+using System;
+using System.Management.Automation;
+
+namespace Microsoft.PowerShell.EditorServices.Extensions
+{
+    /// <summary>
+    /// Provides the entry point of the extensibility API, inserted into
+    /// the PowerShell session as the "$psEditor" variable.
+    /// </summary>
+    public class EditorObject
+    {
+        #region Private Fields
+
+        private ExtensionService extensionService;
+        private IEditorOperations editorOperations;
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// Gets the version of PowerShell Editor Services.
+        /// </summary>
+        public Version EditorServicesVersion
+        {
+            get { return this.GetType().Assembly.GetName().Version; }
+        }
+
+        /// <summary>
+        /// Gets the workspace interface for the editor API.
+        /// </summary>
+        public EditorWorkspace Workspace { get; private set; }
+
+        #endregion
+
+        /// <summary>
+        /// Creates a new instance of the EditorObject class.
+        /// </summary>
+        /// <param name="extensionService">An ExtensionService which handles command registration.</param>
+        /// <param name="editorOperations">An IEditorOperations implementation which handles operations in the host editor.</param>
+        public EditorObject(
+            ExtensionService extensionService,
+            IEditorOperations editorOperations)
+        {
+            this.extensionService = extensionService;
+            this.editorOperations = editorOperations;
+
+            // Create API area objects
+            this.Workspace = new EditorWorkspace(this.editorOperations);
+        }
+
+        /// <summary>
+        /// Registers a new command in the editor.
+        /// </summary>
+        /// <param name="editorCommand">The EditorCommand to be registered.</param>
+        public void RegisterCommand(EditorCommand editorCommand)
+        {
+            this.extensionService.RegisterCommand(editorCommand);
+        }
+
+        /// <summary>
+        /// Unregisters an existing EditorCommand based on its registered name.
+        /// </summary>
+        /// <param name="commandName">The name of the command to be unregistered.</param>
+        public void UnregisterCommand(string commandName)
+        {
+            this.extensionService.UnregisterCommand(commandName);
+        }
+
+        /// <summary>
+        /// Gets the EditorContext which contains the state of the editor
+        /// at the time this method is invoked.
+        /// </summary>
+        /// <returns>A instance of the EditorContext class.</returns>
+        public EditorContext GetEditorContext()
+        {
+            return this.editorOperations.GetEditorContext().Result;
+       }
+    }
+}
+
--- test/PowerShellEditorServices.Test/Session/ScriptFileTests.cs
+++ test/PowerShellEditorServices.Test/Session/ScriptFileTests.cs
@@ -6,11 +6,12 @@
 using Microsoft.PowerShell.EditorServices;
 using System;
 using System.IO;
+using System.Linq;
 using Xunit;
 
 namespace PSLanguageService.Test
 {
-    public class FileChangeTests
+    public class ScriptFileChangeTests
     {
         private static readonly Version PowerShellVersion = new Version("5.0"); 
 
@@ -151,10 +152,28 @@ namespace PSLanguageService.Test
             }
         }
 
-        private void AssertFileChange(
-            string initialString,
-            string expectedString,
-            FileChange fileChange)
+        [Fact]
+        public void ThrowsExceptionWithEditOutsideOfRange()
+        {
+            Assert.Throws(
+                typeof(ArgumentOutOfRangeException),
+                () =>
+                {
+                    this.AssertFileChange(
+                        "first\r\nsecond\r\nREMOVE\r\nTHESE\r\nLINES\r\nthird",
+                        "first\r\nsecond\r\nthird",
+                        new FileChange
+                        {
+                            Line = 3,
+                            EndLine = 7,
+                            Offset = 1,
+                            EndOffset = 1,
+                            InsertString = ""
+                        });
+                });
+        }
+
+        internal static ScriptFile CreateScriptFile(string initialString)
         {
             using (StringReader stringReader = new StringReader(initialString))
             {
@@ -166,10 +185,257 @@ namespace PSLanguageService.Test
                         stringReader,
                         PowerShellVersion);
 
-                // Apply the FileChange and assert the resulting contents
-                fileToChange.ApplyChange(fileChange);
-                Assert.Equal(expectedString, fileToChange.Contents);
+                return fileToChange;
             }
+        }
+
+        private void AssertFileChange(
+            string initialString,
+            string expectedString,
+            FileChange fileChange)
+        {
+            // Create an in-memory file from the StringReader
+            ScriptFile fileToChange = CreateScriptFile(initialString);
+
+            // Apply the FileChange and assert the resulting contents
+            fileToChange.ApplyChange(fileChange);
+            Assert.Equal(expectedString, fileToChange.Contents);
+        }
+    }
+
+    public class ScriptFileGetLinesTests
+    {
+        private ScriptFile scriptFile;
+
+        private const string TestString = "Line One\r\nLine Two\r\nLine Three\r\nLine Four\r\nLine Five";
+        private readonly string[] TestStringLines =
+            TestString.Split(
+                new string[] { "\r\n" },
+                StringSplitOptions.None);
+
+        public ScriptFileGetLinesTests()
+        {
+            this.scriptFile =
+                ScriptFileChangeTests.CreateScriptFile(
+                    "Line One\r\nLine Two\r\nLine Three\r\nLine Four\r\nLine Five\r\n");
+        }
+
+        [Fact]
+        public void CanGetWholeLine()
+        {
+            string[] lines =
+                this.scriptFile.GetLinesInRange(
+                    new BufferRange(5, 1, 5, 10));
+
+            Assert.Equal(1, lines.Length);
+            Assert.Equal("Line Five", lines[0]);
+        }
+
+        [Fact]
+        public void CanGetMultipleWholeLines()
+        {
+            string[] lines =
+                this.scriptFile.GetLinesInRange(
+                    new BufferRange(2, 1, 4, 10));
+
+            Assert.Equal(TestStringLines.Skip(1).Take(3), lines);
+        }
+
+        [Fact]
+        public void CanGetSubstringInSingleLine()
+        {
+            string[] lines =
+                this.scriptFile.GetLinesInRange(
+                    new BufferRange(4, 3, 4, 8));
+
+            Assert.Equal(1, lines.Length);
+            Assert.Equal("ne Fo", lines[0]);
+        }
+
+        [Fact]
+        public void CanGetEmptySubstringRange()
+        {
+            string[] lines =
+                this.scriptFile.GetLinesInRange(
+                    new BufferRange(4, 3, 4, 3));
+
+            Assert.Equal(1, lines.Length);
+            Assert.Equal("", lines[0]);
+        }
+
+        [Fact]
+        public void CanGetSubstringInMultipleLines()
+        {
+            string[] expectedLines = new string[]
+            {
+                "Two",
+                "Line Three",
+                "Line Fou"
+            };
+
+            string[] lines =
+                this.scriptFile.GetLinesInRange(
+                    new BufferRange(2, 6, 4, 9));
+
+            Assert.Equal(expectedLines, lines);
+        }
+
+        [Fact]
+        public void CanGetRangeAtLineBoundaries()
+        {
+            string[] expectedLines = new string[]
+            {
+                "",
+                "Line Three",
+                ""
+            };
+
+            string[] lines =
+                this.scriptFile.GetLinesInRange(
+                    new BufferRange(2, 9, 4, 1));
+
+            Assert.Equal(expectedLines, lines);
+        }
+    }
+
+    public class ScriptFilePositionTests
+    {
+        private ScriptFile scriptFile;
+
+        public ScriptFilePositionTests()
+        {
+            this.scriptFile =
+                ScriptFileChangeTests.CreateScriptFile(@"
+First line
+  Second line is longer
+    Third line
+");
+        }
+
+        [Fact]
+        public void CanOffsetByLine()
+        {
+            this.AssertNewPosition(
+                1, 1,
+                2, 0,
+                3, 1);
+
+            this.AssertNewPosition(
+                3, 1,
+                -2, 0,
+                1, 1);
+        }
+
+        [Fact]
+        public void CanOffsetByColumn()
+        {
+            this.AssertNewPosition(
+                2, 1,
+                0, 2,
+                2, 3);
+
+            this.AssertNewPosition(
+                2, 5,
+                0, -3,
+                2, 2);
+        }
+
+        [Fact]
+        public void ThrowsWhenPositionOutOfRange()
+        {
+            // Less than line range
+            Assert.Throws(
+                typeof(ArgumentOutOfRangeException),
+                () =>
+                {
+                    scriptFile.CalculatePosition(
+                        new BufferPosition(1, 1),
+                        -10, 0);
+                });
+
+            // Greater than line range
+            Assert.Throws(
+                typeof(ArgumentOutOfRangeException),
+                () =>
+                {
+                    scriptFile.CalculatePosition(
+                        new BufferPosition(1, 1),
+                        10, 0);
+                });
+
+            // Less than column range
+            Assert.Throws(
+                typeof(ArgumentOutOfRangeException),
+                () =>
+                {
+                    scriptFile.CalculatePosition(
+                        new BufferPosition(1, 1),
+                        0, -10);
+                });
+
+            // Greater than column range
+            Assert.Throws(
+                typeof(ArgumentOutOfRangeException),
+                () =>
+                {
+                    scriptFile.CalculatePosition(
+                        new BufferPosition(1, 1),
+                        0, 10);
+                });
+        }
+
+        [Fact]
+        public void CanFindBeginningOfLine()
+        {
+            this.AssertNewPosition(
+                4, 12,
+                pos => pos.GetLineStart(),
+                4, 5);
+        }
+
+        [Fact]
+        public void CanFindEndOfLine()
+        {
+            this.AssertNewPosition(
+                4, 12,
+                pos => pos.GetLineEnd(),
+                4, 15);
+        }
+
+        [Fact]
+        public void CanComposePositionOperations()
+        {
+            this.AssertNewPosition(
+                4, 12,
+                pos => pos.AddOffset(-1, 1).GetLineStart(),
+                3, 3);
+        }
+
+        private void AssertNewPosition(
+            int originalLine, int originalColumn,
+            int lineOffset, int columnOffset,
+            int expectedLine, int expectedColumn)
+        {
+            this.AssertNewPosition(
+                originalLine, originalColumn,
+                pos => pos.AddOffset(lineOffset, columnOffset),
+                expectedLine, expectedColumn);
+        }
+
+        private void AssertNewPosition(
+            int originalLine, int originalColumn,
+            Func<FilePosition, FilePosition> positionOperation,
+            int expectedLine, int expectedColumn)
+        {
+            var newPosition =
+                positionOperation(
+                    new FilePosition(
+                        this.scriptFile,
+                        originalLine,
+                        originalColumn));
+
+            Assert.Equal(expectedLine, newPosition.Line);
+            Assert.Equal(expectedColumn, newPosition.Column);
         }
     }
 }
