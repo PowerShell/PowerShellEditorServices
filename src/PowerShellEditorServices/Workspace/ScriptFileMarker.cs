5304a268634a1cd460bb396a4833e998f6cc8ff0
David Wilson <david@daviwil.com>
2016-12-07 14:38:08 -0800

    Add documentation comments for the MarkerCorreciton class

--- src/PowerShellEditorServices/Workspace/ScriptFileMarker.cs
+++ src/PowerShellEditorServices/Workspace/ScriptFileMarker.cs
@@ -12,10 +12,19 @@ using System.Management.Automation.Language;
 
 namespace Microsoft.PowerShell.EditorServices
 {
+    /// <summary>
+    /// Contains details for a code correction which can be applied from a ScriptFileMarker.
+    /// </summary>
     public class MarkerCorrection
     {
+        /// <summary>
+        /// Gets or sets the display name of the code correction.
+        /// </summary>
         public string Name { get; set; }
 
+        /// <summary>
+        /// Gets or sets the list of ScriptRegions that define the edits to be made by the correction.
+        /// </summary>
         public ScriptRegion[] Edits { get; set; }
     }
 
@@ -64,6 +73,9 @@ namespace Microsoft.PowerShell.EditorServices
         /// </summary>
         public ScriptRegion ScriptRegion { get; set; }
 
+        /// <summary>
+        /// Gets or sets an optional code correction that can be applied based on this marker.
+        /// </summary>
         public MarkerCorrection Correction { get; set; }
 
         #endregion
