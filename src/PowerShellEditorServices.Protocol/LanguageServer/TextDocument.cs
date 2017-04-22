f7e05e33f9b3051f40f32e58a483075cb4fd9f2e
Kapil Borle <kborle@microsoft.com>
2017-04-21 18:51:03 -0700

    Add VersionedTextDocumentIdentifier type

--- src/PowerShellEditorServices.Protocol/LanguageServer/TextDocument.cs
+++ src/PowerShellEditorServices.Protocol/LanguageServer/TextDocument.cs
@@ -55,6 +55,17 @@ namespace Microsoft.PowerShell.EditorServices.Protocol.LanguageServer
     }
 
     /// <summary>
+    /// An identifier to denote a specific version of a text document.
+    /// </summary>
+    public class VersionedTextDocumentIdentifier : TextDocumentIdentifier
+    {
+        /// <summary>
+        /// The version number of this document.
+        /// </summary>
+        public int Version { get; set; }
+    }
+
+    /// <summary>
     /// Defines a position in a text document.
     /// </summary>
     [DebuggerDisplay("TextDocumentPosition = {Position.Line}:{Position.Character}")]
