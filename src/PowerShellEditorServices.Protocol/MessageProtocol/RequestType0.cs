eb997017eb070b605a92aa658f3df11dcd18213e
Kapil Borle <kborle@microsoft.com>
2017-04-26 17:05:52 -0700

    Add missing RequestType0 file

---
+++ src/PowerShellEditorServices.Protocol/MessageProtocol/RequestType0.cs
@@ -0,0 +1,22 @@
+﻿//
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+//
+
+using System.Diagnostics;
+
+namespace Microsoft.PowerShell.EditorServices.Protocol.MessageProtocol
+{
+    [DebuggerDisplay("RequestType0 Method = {Method}")]
+    public class RequestType0<TResult, TError, TRegistrationOption> : AbstractMessageType
+    {
+        public RequestType0(string method) : base(method, 0)
+        {
+        }
+
+        public static RequestType0<TResult, TError, TRegistrationOption> Create(string method)
+        {
+            return new RequestType0<TResult, TError, TRegistrationOption>(method);
+        }
+    }
+}
