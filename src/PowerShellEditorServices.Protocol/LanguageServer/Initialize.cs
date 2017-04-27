d74dbc6847d7a3c1595ad87a44df198f5dbed85f
Kapil Borle <kborle@microsoft.com>
2017-04-27 16:06:05 -0700

    Add InitializeParams type

--- src/PowerShellEditorServices.Protocol/LanguageServer/Initialize.cs
+++ src/PowerShellEditorServices.Protocol/LanguageServer/Initialize.cs
@@ -10,20 +10,53 @@ namespace Microsoft.PowerShell.EditorServices.Protocol.LanguageServer
     public class InitializeRequest
     {
         public static readonly
-            RequestType<InitializeRequest, InitializeResult, object, object> Type =
-            RequestType<InitializeRequest, InitializeResult, object, object>.Create("initialize");
+            RequestType<InitializeParams, InitializeResult, object, object> Type =
+            RequestType<InitializeParams, InitializeResult, object, object>.Create("initialize");
+   }
+
+    public enum TraceType {
+        Off,
+        Messages,
+        Verbose
+    }
+
+    public class InitializeParams {
+        /// <summary>
+        /// The process Id of the parent process that started the server
+        /// </summary>
+        public int ProcessId { get; set; }
 
         /// <summary>
-        /// Gets or sets the root path of the editor's open workspace.
-        /// If null it is assumed that a file was opened without having
-        /// a workspace open.
+        /// The root path of the workspace. It is null if no folder is open.
+        ///
+        /// This property has been deprecated in favor of RootUri.
         /// </summary>
         public string RootPath { get; set; }
 
         /// <summary>
-        /// Gets or sets the capabilities provided by the client (editor).
+        /// The root uri of the workspace. It is null if not folder is open. If both
+        /// `RootUri` and `RootPath` are non-null, `RootUri` should be used.
+        /// </summary>
+        public string RootUri { get; set; }
+
+        /// <summary>
+        /// The capabilities provided by the client.
         /// </summary>
         public ClientCapabilities Capabilities { get; set; }
+
+        /// <summary>
+        /// User provided initialization options.
+        ///
+        /// This is defined as `any` type on the client side.
+        /// </summary>
+        public object InitializeOptions { get; set; }
+
+        // TODO We need to verify if the deserializer will map the type defined in the client
+        // to an enum.
+        /// <summary>
+        /// The initial trace setting. If omitted trace is disabled.
+        /// </summary>
+        public TraceType Trace { get; set; } = TraceType.Off;
     }
 
     public class InitializeResult
