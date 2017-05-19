69f48e9cacc238c310d9500b6852476f19a3756c
Kapil Borle <kborle@microsoft.com>
2017-05-18 19:12:03 -0700

    Add test to find variable defn with special chars in name

--- test/PowerShellEditorServices.Test.Host/TestFiles/FindReferences.ps1
+++ test/PowerShellEditorServices.Test.Host/TestFiles/FindReferences.ps1
@@ -14,3 +14,6 @@ Write-Output ""
 
 . .\VariableDefinition.ps1
 Write-Output $variableInOtherFile
+
+${variable-with-weird-name} = "this variable has special characters"
+Write-Output ${variable-with-weird-name}
