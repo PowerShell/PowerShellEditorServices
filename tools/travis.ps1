8734df9abe4776db48c37a1e1a2389bad9bb2227
Mark E. Schill <Mark.Schill@cmschill.net>
2017-05-23 22:36:41 -0400

    Enable Travis CI with Ubuntu and MacOS builds
    - Set to Trusty Tahr (Ubuntu 14.04)
    - Set to xcode 8.3 (OS X 10.12)
    - Downloads and installs PowerShell v6.0.0-beta.1
    - Performs tests as part of the build process

---
+++ tools/travis.ps1
@@ -0,0 +1,9 @@
+
+
+# Install InvokeBuild
+Install-Module InvokeBuild -Scope CurrentUser -Force
+
+
+# Build the code and perform tests
+Import-module InvokeBuild
+Invoke-Build -Configuration Release
