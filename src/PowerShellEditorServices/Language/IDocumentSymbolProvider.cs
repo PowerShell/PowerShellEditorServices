04113a3889f4cd76ce7fa57dd5eddeedaef60732
Kapil Borle <kborle@microsoft.com>
2017-06-03 14:36:30 -0700

    Use document provider interface to provide symbols

---
+++ src/PowerShellEditorServices/Language/IDocumentSymbolProvider.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+
+namespace Microsoft.PowerShell.EditorServices
+{
+    internal interface IDocumentSymbolProvider
+    {
+        IEnumerable<SymbolReference> GetSymbols(ScriptFile scriptFile, Version psVersion = null);
+        bool CanProvideFor(ScriptFile scriptFile);
+    }
+}
