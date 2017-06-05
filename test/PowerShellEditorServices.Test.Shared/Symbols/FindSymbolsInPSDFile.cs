c6271653dce470319384974430ccc36f34cb1e22
Kapil Borle <kborle@microsoft.com>
2017-06-04 23:56:47 -0700

    Add relevant test files

---
+++ test/PowerShellEditorServices.Test.Shared/Symbols/FindSymbolsInPSDFile.cs
@@ -0,0 +1,17 @@
+//
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+//
+
+namespace Microsoft.PowerShell.EditorServices.Test.Shared.Symbols
+{
+    public class FindSymbolsInPSDFile
+    {
+        public static readonly ScriptRegion SourceDetails =
+            new ScriptRegion
+            {
+                File = @"Symbols\PowerShellDataFile.psd1"
+            };
+    }
+}
+
