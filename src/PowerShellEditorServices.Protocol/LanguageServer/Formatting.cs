78ad540c2720834671d2b42b04c86342c578cf3e
Kapil Borle <kborle@microsoft.com>
2017-06-06 17:49:44 -0700

    Add formatting related types

---
+++ src/PowerShellEditorServices.Protocol/LanguageServer/Formatting.cs
@@ -0,0 +1,28 @@
+//
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+//
+
+using Microsoft.PowerShell.EditorServices.Protocol.MessageProtocol;
+
+namespace Microsoft.PowerShell.EditorServices.Protocol.LanguageServer
+{
+    public class DocumentFormattingRequest
+    {
+        public static readonly
+            RequestType<DocumentFormattingParams, TextEdit[], object,TextDocumentRegistrationOptions> Type = RequestType<DocumentFormattingParams, TextEdit[], object,TextDocumentRegistrationOptions>.Create("textDocument/definition");
+    }
+
+    public class DocumentFormattingParams
+    {
+        public TextDocumentIdentifier TextDocument { get; set; }
+        public FormattingOptions options { get; set; }
+    }
+
+    public class FormattingOptions
+    {
+        int TabSize { get; set; }
+        bool InsertSpaces { get; set; }
+    }
+}
+
