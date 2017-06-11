793d5bcb503c7d9264edaa93cb076bf5280ded77
David Wilson <daviwil@microsoft.com>
2017-06-11 09:53:05 -0700

    Fix #509: Suppress missing language errors from Import-LocalizedData

--- module/PowerShellEditorServices/Commands/PowerShellEditorServices.Commands.psm1
+++ module/PowerShellEditorServices/Commands/PowerShellEditorServices.Commands.psm1
@@ -1,4 +1,4 @@
-Import-LocalizedData -BindingVariable Strings -FileName Strings
+Import-LocalizedData -BindingVariable Strings -FileName Strings -ErrorAction Ignore
 
 Get-ChildItem -Path $PSScriptRoot\Public\*.ps1 | ForEach-Object {
     . $PSItem.FullName
