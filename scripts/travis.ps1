781df1ce3ac0e45abca9dbcdeb1a70fd2cb99287
Tyler Leonhardt <tylerl0706@gmail.com>
2018-01-02 15:04:25 -0800

    add maximum versions

--- scripts/travis.ps1
+++ scripts/travis.ps1
@@ -1,8 +1,8 @@
 
 
 # Install InvokeBuild
-Install-Module InvokeBuild -Scope CurrentUser -Force
-Install-Module PlatyPS -Scope CurrentUser -Force
+Install-Module InvokeBuild -MaximumVersion 5.1.0 -Scope CurrentUser -Force
+Install-Module PlatyPS -MaximumVersion 0.10.0 -Scope CurrentUser -Force
 
 
 # Build the code and perform tests
