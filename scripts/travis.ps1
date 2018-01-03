0e849e21277a1a5628ee01cfa3c82401001992d1
Tyler James Leonhardt <tylerl0706@gmail.com>
2018-01-02 18:51:50 -0800

    change 'job Test -Safe' to '?Test' (#595)
    * change 'job Test -Safe' to '?Test'
    
    * see if AppVeyor can handle no required version
    
    * add maximum versions
    
    * maximum, not required. typo
    
    * switch platyps back to requiredversion
    
    * consistant platyps version

--- scripts/travis.ps1
+++ scripts/travis.ps1
@@ -1,8 +1,8 @@
 
 
 # Install InvokeBuild
-Install-Module InvokeBuild -Scope CurrentUser -Force
-Install-Module PlatyPS -Scope CurrentUser -Force
+Install-Module InvokeBuild -MaximumVersion 5.1.0 -Scope CurrentUser -Force
+Install-Module PlatyPS -RequiredVersion 0.9.0 -Scope CurrentUser -Force
 
 
 # Build the code and perform tests
