b3d067adeb980018d66aef58419402d4153e27ab
Tyler Leonhardt <tylerl0706@gmail.com>
2018-01-02 16:40:37 -0800

    consistant platyps version

--- scripts/travis.ps1
+++ scripts/travis.ps1
@@ -2,7 +2,7 @@
 
 # Install InvokeBuild
 Install-Module InvokeBuild -MaximumVersion 5.1.0 -Scope CurrentUser -Force
-Install-Module PlatyPS -MaximumVersion 0.10.0 -Scope CurrentUser -Force
+Install-Module PlatyPS -RequiredVersion 0.9.0 -Scope CurrentUser -Force
 
 
 # Build the code and perform tests
