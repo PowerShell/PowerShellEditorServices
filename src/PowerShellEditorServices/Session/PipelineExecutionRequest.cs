afdfb438e5c8c254d1fde92c4fc81d815b9cea44
Patrick Meinecke <seeminglyscience@gmail.com>
2018-06-09 10:24:02 -0400

    add a bunch of copyright headers I missed

--- src/PowerShellEditorServices/Session/PipelineExecutionRequest.cs
+++ src/PowerShellEditorServices/Session/PipelineExecutionRequest.cs
@@ -1,3 +1,8 @@
+//
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+//
+
 using System.Collections.Generic;
 using System.Management.Automation;
 using System.Text;
