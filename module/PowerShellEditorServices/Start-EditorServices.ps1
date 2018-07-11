eecfe026d9bbd50557a4c9f5332e6c95789b0096
Robert Holt <rjmholt@gmail.com>
2018-07-11 10:13:47 -0700

    Fix issue with stat format on Linux (#698)
    * Fix issue with stat format on Linux
    
    * Improve macOS differentiation

--- module/PowerShellEditorServices/Start-EditorServices.ps1
+++ module/PowerShellEditorServices/Start-EditorServices.ps1
@@ -222,13 +222,20 @@ function Set-NamedPipeMode {
         [string]
         $PipeFile
     )
-    chmod $DEFAULT_USER_MODE $PipeFile
-    if ($IsLinux) {
-        $mode = stat -c "%A" $PipeFile
+
+    if ($IsWindows) {
+        return
     }
-    else {
+
+    chmod $DEFAULT_USER_MODE $PipeFile
+
+    if ($IsLinux) {
+        $mode = stat -c "%a" $PipeFile
+    }
+    elseif ($IsMacOS) {
         $mode = stat -f "%A" $PipeFile
     }
+
     if ($mode -ne $DEFAULT_USER_MODE) {
         ExitWithError "Permissions to the pipe file were not set properly. Expected: $DEFAULT_USER_MODE Actual: $mode for file: $PipeFile"
     }
