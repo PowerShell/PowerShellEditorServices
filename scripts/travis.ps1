277713921f5aad6e005d3804ce0d218dd734add4
Robert Holt <roholt@microsoft.com>
2018-09-10 20:57:05 -0700

    Make travis installation user-local

--- scripts/travis.ps1
+++ scripts/travis.ps1
@@ -1,6 +1,6 @@
 Get-Module PowerShellGet,PackageManagement | Remove-Module -Force -Verbose
-powershell -Command { Install-Module -Name PowershellGet -MinimumVersion 1.6 -force -confirm:$false -verbose }
-powershell -Command { Install-Module -Name PackageManagement -MinimumVersion 1.1.7.0 -Force -Confirm:$false -Verbose }
+powershell -Command { Install-Module -Name PowershellGet -MinimumVersion 1.6 -Scope CurrentUser -force -confirm:$false -verbose }
+powershell -Command { Install-Module -Name PackageManagement -MinimumVersion 1.1.7.0 -Scope CurrentUser -Force -Confirm:$false -Verbose }
 Import-Module -Name PowerShellGet -MinimumVersion 1.6 -Force
 Import-Module -Name PackageManagement -MinimumVersion 1.1.7.0 -Force
 Install-PackageProvider -Name NuGet -Force | Out-Null
