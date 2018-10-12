f1fa0f2546bf311aea611a7aa02a2be9e4d6118b
Robert Holt <roholt@microsoft.com>
2018-10-12 16:00:17 -0700

    Add default BuildInfo file

---
+++ src/PowerShellEditorServices.Host/BuildInfo/BuildInfo.cs
@@ -0,0 +1,9 @@
+namespace Microsoft.PowerShell.EditorServices.Host
+{
+    public static class BuildInfo
+    {
+        public const string BuildVersion = "<unset>";
+        public const string BuildOrigin = "<unset>";
+        public static readonly DateTime? BuildTime = null;
+    }
+}
