7a854609be13992b3962420c9885401f51f93b43
Patrick Meinecke <SeeminglyScience@users.noreply.github.com>
2018-11-29 17:39:51 -1000

    Update src/PowerShellEditorServices/Session/RunspaceDetails.cs
    Co-Authored-By: TylerLeonhardt <tylerl0706@gmail.com>

--- src/PowerShellEditorServices/Session/RunspaceDetails.cs
+++ src/PowerShellEditorServices/Session/RunspaceDetails.cs
@@ -221,7 +221,7 @@ namespace Microsoft.PowerShell.EditorServices.Session
                 // Grab the $host.name which will tell us if we're in a PSRP session or not
                 string hostname =
                         PowerShellContext.ExecuteScriptAndGetItem<string>(
-                            "$host.name",
+                            "$Host.Name",
                             runspace,
                             defaultValue: string.Empty);
 
