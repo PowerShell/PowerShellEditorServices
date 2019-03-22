0e05c920076bad0174ed125af5172c8a817c60f1
Tyler Leonhardt <tylerl0706@gmail.com>
2019-03-22 13:29:55 -0700

    back to empty list since we supply parse errors in PSES again

--- test/PowerShellEditorServices.Test/Session/ScriptFileTests.cs
+++ test/PowerShellEditorServices.Test/Session/ScriptFileTests.cs
@@ -550,7 +550,7 @@ First line
             Assert.True(scriptFile.IsAnalysisEnabled);
             Assert.False(scriptFile.IsInMemory);
             Assert.Empty(scriptFile.ReferencedFiles);
-            Assert.Null(scriptFile.SyntaxMarkers);
+            Assert.Empty(scriptFile.DiagnosticMarkers);
             Assert.Single(scriptFile.ScriptTokens);
             Assert.Single(scriptFile.FileLines);
         }
@@ -576,7 +576,7 @@ First line
                 Assert.True(scriptFile.IsAnalysisEnabled);
                 Assert.True(scriptFile.IsInMemory);
                 Assert.Empty(scriptFile.ReferencedFiles);
-                Assert.Null(scriptFile.SyntaxMarkers);
+                Assert.Empty(scriptFile.DiagnosticMarkers);
                 Assert.Equal(10, scriptFile.ScriptTokens.Length);
                 Assert.Equal(3, scriptFile.FileLines.Count);
             }
