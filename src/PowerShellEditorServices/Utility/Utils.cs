ebbd1c6fd506f139ae2538f8bd36d014c64b82bd
Robert Holt <rjmholt_msft@outlook.com>
2019-04-09 09:55:09 -0700

    Update Utils.cs

--- src/PowerShellEditorServices/Utility/Utils.cs
+++ src/PowerShellEditorServices/Utility/Utils.cs
@@ -3,10 +3,6 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 //
 
-#if CoreCLR
-using System.Runtime.InteropServices;
-#endif
-
 namespace Microsoft.PowerShell.EditorServices
 {
     /// <summary>
