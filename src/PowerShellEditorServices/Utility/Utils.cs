1dd99018c841c9c7555a8f56fd5e6f5984b2db9d
Patrick Meinecke <SeeminglyScience@users.noreply.github.com>
2019-04-09 09:19:29 -0700

    Update src/PowerShellEditorServices/Utility/Utils.cs
    Co-Authored-By: rjmholt <rjmholt@gmail.com>

--- src/PowerShellEditorServices/Utility/Utils.cs
+++ src/PowerShellEditorServices/Utility/Utils.cs
@@ -3,6 +3,7 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 //
 
+using System;
 using System.Runtime.InteropServices;
 
 namespace Microsoft.PowerShell.EditorServices
