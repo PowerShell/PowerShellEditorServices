02a6bd9abe9f4a4bcd427b202b1518589e993d3f
Patrick Meinecke <SeeminglyScience@users.noreply.github.com>
2019-04-12 12:24:35 -0700

    Patrick's feedback
    Co-Authored-By: TylerLeonhardt <tylerl0706@gmail.com>

--- scripts/azurePipelinesBuild.ps1
+++ scripts/azurePipelinesBuild.ps1
@@ -1,10 +1,10 @@
 $ErrorActionPreference = 'Stop'
 
 Set-PSRepository -Name PSGallery -InstallationPolicy Trusted | Out-Null
-if($IsWindows -or $PSVersionTable.PSVersion.Major -lt 6) {
+if ($IsWindows -or $PSVersionTable.PSVersion.Major -lt 6) {
     # We rely on PowerShellGet's -AllowPrerelease which is in PowerShellGet 1.6 so we need to update PowerShellGet.
     Get-Module PowerShellGet,PackageManagement | Remove-Module -Force -Verbose
-    powershell -Command { Install-Module -Name PowershellGet -MinimumVersion 1.6 -Force -confirm:$false -verbose }
+    powershell -Command { Install-Module -Name PowerShellGet -MinimumVersion 1.6 -Force -Confirm:$false -Verbose }
     powershell -Command { Install-Module -Name PackageManagement -MinimumVersion 1.1.7.0 -Force -Confirm:$false -Verbose }
     Import-Module -Name PowerShellGet -MinimumVersion 1.6 -Force
     Import-Module -Name PackageManagement -MinimumVersion 1.1.7.0 -Force
