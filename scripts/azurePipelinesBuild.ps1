84064159349550f85b6a6b8abb248f25c9870de0
Tyler Leonhardt <tylerl0706@gmail.com>
2019-04-11 17:26:22 -0700

    add job name

--- scripts/azurePipelinesBuild.ps1
+++ scripts/azurePipelinesBuild.ps1
@@ -1,19 +1,17 @@
 $ErrorActionPreference = 'Stop'
 
-# Needed for build and docs gen.
-Install-Module InvokeBuild -MaximumVersion 5.1.0 -Scope CurrentUser -Force
-Install-Module PlatyPS -RequiredVersion 0.9.0 -Scope CurrentUser -Force
-
+Set-PSRepository -Name PSGallery -InstallationPolicy Trusted | Out-Null
 if($IsWindows -or $PSVersionTable.PSVersion.Major -lt 6) {
     # We rely on PowerShellGet's -AllowPrerelease which is in PowerShellGet 1.6 so we need to update PowerShellGet.
     Get-Module PowerShellGet,PackageManagement | Remove-Module -Force -Verbose
-    powershell -Command { Install-Module -Name PowershellGet -MinimumVersion 1.6 -force -confirm:$false -verbose }
+    powershell -Command { Install-Module -Name PowershellGet -MinimumVersion 1.6 -Force -confirm:$false -verbose }
     powershell -Command { Install-Module -Name PackageManagement -MinimumVersion 1.1.7.0 -Force -Confirm:$false -Verbose }
     Import-Module -Name PowerShellGet -MinimumVersion 1.6 -Force
     Import-Module -Name PackageManagement -MinimumVersion 1.1.7.0 -Force
-    Install-PackageProvider -Name NuGet -Force | Out-Null
-    Import-PackageProvider NuGet -Force | Out-Null
-    Set-PSRepository -Name PSGallery -InstallationPolicy Trusted | Out-Null
 }
+
+# Needed for build and docs gen.
+Install-Module InvokeBuild -MaximumVersion 5.1.0 -Scope CurrentUser
+Install-Module PlatyPS -RequiredVersion 0.9.0 -Scope CurrentUser
 
 Invoke-Build -Configuration Release
