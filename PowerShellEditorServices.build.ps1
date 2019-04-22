be4587e78ec24aa32bb9485cac6e9fb672da488d
Robert Holt <rjmholt_msft@outlook.com>
2019-04-23 08:45:16 +1000

    Add pester installation to build step

--- PowerShellEditorServices.build.ps1
+++ PowerShellEditorServices.build.ps1
@@ -376,10 +376,19 @@ task TestHost {
     exec { & $script:dotnetExe test -f $script:TestRuntime.Core (DotNetTestFilter) }
 }
 
-task TestPester -After Build,BuildPsesClientModule {
+task TestPester -After Build,BuildPsesClientModule,EnsurePesterInstalled {
     $pwshExe = (Get-Process -Id $PID).Path
     $pesterTestDir = Resolve-Path "$PSScriptRoot/test/Pester/"
     exec { & $pwshExe -Command "cd $pesterTestDir; Invoke-Pester" }
+}
+
+task EnsurePesterInstalled {
+    if (Get-Command Invoke-Pester -ErrorAction SilentlyContinue)
+    {
+        return
+    }
+
+    Install-Module -Scope CurrentUser Pester
 }
 
 task LayoutModule -After Build {
