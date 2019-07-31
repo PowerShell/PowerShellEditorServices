b8ac4f03cee6b8ec647a9172cc11a7e39d5add62
Robert Holt <rjmholt_msft@outlook.com>
2019-07-31 12:46:33 -0700

    Add handler scaffold

---
+++ src/PowerShellEditorServices.Engine/Services/TextDocument/Handlers/DocumentHighlightHandler.cs
@@ -0,0 +1,12 @@
+namespace PowerShellEditorServices.Engine.Services.Handlers
+{
+    public class DocumentHighlightHandler : DocumentHighlightHandler
+    {
+        public override Task<DocumentHighlightContainer> Handle(
+            DocumentHighlightParams request,
+            CancellationToken cancellationToken)
+        {
+
+        }
+    }
+}
