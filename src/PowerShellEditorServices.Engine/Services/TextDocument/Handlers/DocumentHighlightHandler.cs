81f3e6999efb0da38ba285bb455a3ce24cb61620
Robert Holt <rjmholt_msft@outlook.com>
2019-07-31 13:11:19 -0700

    More stuff

--- src/PowerShellEditorServices.Engine/Services/TextDocument/Handlers/DocumentHighlightHandler.cs
+++ src/PowerShellEditorServices.Engine/Services/TextDocument/Handlers/DocumentHighlightHandler.cs
@@ -2,6 +2,19 @@ namespace PowerShellEditorServices.Engine.Services.Handlers
 {
     public class DocumentHighlightHandler : DocumentHighlightHandler
     {
+        private readonly ILogger _logger;
+        private readonly WorkspaceService _workspaceService;
+
+        public DocumentHighlightHandler(
+            ILoggerFactory loggerFactory,
+            WorkspaceService workspaceService,
+            TextDocumentRegistrationOptions registrationOptions)
+            : base(options)
+        {
+            _logger = loggerFactory.CreateLogger<DocumentHighlightHandler>();
+            _workspaceService = workspaceService;
+        }
+
         public override Task<DocumentHighlightContainer> Handle(
             DocumentHighlightParams request,
             CancellationToken cancellationToken)
