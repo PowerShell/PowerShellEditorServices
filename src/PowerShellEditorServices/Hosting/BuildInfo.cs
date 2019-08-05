d6227645988fe52f459a4e744f2eaca29142a0bf
Tyler James Leonhardt <tylerl0706@gmail.com>
2019-08-05 12:42:57 -0700

    Test only pester for now (#1003)

---
+++ src/PowerShellEditorServices/Hosting/BuildInfo.cs
@@ -0,0 +1,9 @@
+namespace Microsoft.PowerShell.EditorServices.Hosting
+{
+    public static class BuildInfo
+    {
+        public const string BuildVersion = "<development-build>";
+        public const string BuildOrigin = "<development>";
+        public static readonly System.DateTime? BuildTime = System.DateTime.Parse("2019-10-03T13:14:51");
+    }
+}
