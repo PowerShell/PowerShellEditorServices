cd0e7bc1b2a2a85b986d8ff85629e301f01fd6f1
Robert Holt <rjmholt_msft@outlook.com>
2019-08-19 16:45:47 -0700

    Fix crash when setBreakpoint from VSCode sends a git:/ URI... (#1000)
    * Fix git uri problem
    
    * Remove unused imports

--- src/PowerShellEditorServices/Workspace/Workspace.cs
+++ src/PowerShellEditorServices/Workspace/Workspace.cs
@@ -29,6 +29,13 @@ namespace Microsoft.PowerShell.EditorServices
             "*.psd1"
         };
 
+        private static readonly string[] s_supportedUriSchemes = new[]
+        {
+            "file",
+            "untitled",
+            "inmemory"
+        };
+
         private ILogger logger;
         private Version powerShellVersion;
         private Dictionary<string, ScriptFile> workspaceFiles = new Dictionary<string, ScriptFile>();
@@ -142,6 +149,20 @@ namespace Microsoft.PowerShell.EditorServices
         /// <param name="scriptFile">The out parameter that will contain the ScriptFile object.</param>
         public bool TryGetFile(string filePath, out ScriptFile scriptFile)
         {
+            try
+            {
+                if (filePath.Contains(":/") // Quick heuristic to determine if we might have a URI
+                    && !s_supportedUriSchemes.Contains(new Uri(filePath).Scheme))
+                {
+                    scriptFile = null;
+                    return false;
+                }
+            }
+            catch
+            {
+                // If something goes wrong trying to check for URIs, just proceed to normal logic
+            }
+
             try
             {
                 scriptFile = GetFile(filePath);
