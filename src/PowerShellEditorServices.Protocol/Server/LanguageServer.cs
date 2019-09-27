bf5d7656e2d03c579ccea47212794c51d6a7a281
Tyler Leonhardt <tylerl0706@gmail.com>
2019-09-27 09:00:33 -0700

    here everywhere

--- src/PowerShellEditorServices.Protocol/Server/LanguageServer.cs
+++ src/PowerShellEditorServices.Protocol/Server/LanguageServer.cs
@@ -2014,8 +2014,8 @@ function __Expand-Alias {
                 // Since we want to use a "tab stop" we need to escape a few things for Textmate to render properly.
                 var sb = new StringBuilder(completionDetails.CompletionText)
                     .Replace(@"\", @"\\")
-                    .Replace("}", "\\}")
-                    .Replace("$", "\\$");
+                    .Replace(@"}", @"\}")
+                    .Replace(@"$", @"\$");
                 completionText = sb.Insert(sb.Length - 1, "$0").ToString();
                 insertTextFormat = InsertTextFormat.Snippet;
             }
