40aadf783f5300e2475b12ee9764ddf38f8a3082
Tyler Leonhardt <tylerl0706@gmail.com>
2019-10-03 14:08:52 -0700

    delete buildinfo

--- src/PowerShellEditorServices/Hosting/BuildInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace Microsoft.PowerShell.EditorServices.Hosting
-{
-    public static class BuildInfo
-    {
-        public const string BuildVersion = "<development-build>";
-        public const string BuildOrigin = "<development>";
-        public static readonly System.DateTime? BuildTime = System.DateTime.Parse("2019-10-03T13:14:51");
-    }
-}
