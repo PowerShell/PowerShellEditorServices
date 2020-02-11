17587f7c837d7e281e0c442112c252dbf07812a1
Rob Holt <roholt@microsoft.com>
2020-02-11 14:34:22 -0800

    Add APIs to get and load through the PSES ALC

--- src/PowerShellEditorServices/Extensions/EngineExtensions/EditorEngine.cs
+++ src/PowerShellEditorServices/Extensions/EngineExtensions/EditorEngine.cs
@@ -5,6 +5,7 @@ using OmniSharp.Extensions.LanguageServer.Protocol.Server;
 using System;
 using System.Linq.Expressions;
 using System.Reflection;
+using System.Runtime.InteropServices;
 
 namespace Microsoft.PowerShell.EditorServices.Extensions
 {
@@ -12,9 +13,21 @@ namespace Microsoft.PowerShell.EditorServices.Extensions
     {
         private static readonly Assembly s_psesAsm = typeof(EditorEngine).Assembly;
 
+        private static readonly Lazy<object> s_psesAsmLoadContextLazy = new Lazy<object>(GetPsesAsmLoadContext);
+
+        private static readonly Lazy<Type> s_asmLoadContextType = new Lazy<Type>(() => Type.GetType("System.Runtime.Loader.AssemblyLoadContext"));
+
         private static readonly Lazy<Func<IDisposable>> s_enterPsesReflectionContextLazy = new Lazy<Func<IDisposable>>(GetPsesAlcReflectionContextEntryFunc);
 
+        private static readonly Lazy<Func<string, Assembly>> s_loadAssemblyInPsesAlc = new Lazy<Func<string, Assembly>>(GetPsesAlcLoadAsmFunc);
+
+        private static Type AsmLoadContextType => s_asmLoadContextType.Value;
+
+        private static object PsesAssemblyLoadContext => s_psesAsmLoadContextLazy.Value;
+
         private static Func<IDisposable> EnterPsesAlcReflectionContext => s_enterPsesReflectionContextLazy.Value;
+
+        private static Func<string, Assembly> LoadAssemblyInPsesAlc => s_loadAssemblyInPsesAlc.Value;
 
         private readonly IServiceProvider _serviceProvider;
 
@@ -42,7 +55,7 @@ namespace Microsoft.PowerShell.EditorServices.Extensions
         public object GetServiceByAssemblyQualifiedName(string asmQualifiedTypeName)
         {
             Type serviceType;
-            if (PowerShellReflectionUtils.PSVersion.Major >= 6)
+            if (VersionUtils.IsNetCore)
             {
                 using (EnterPsesAlcReflectionContext())
                 {
@@ -57,17 +70,48 @@ namespace Microsoft.PowerShell.EditorServices.Extensions
             return _serviceProvider.GetService(serviceType);
         }
 
+        public object GetPsesAssemblyLoadContext()
+        {
+            return PsesAssemblyLoadContext;
+        }
+
+        public Assembly LoadAssemblyInPsesLoadContext(string assemblyPath)
+        {
+            if (!VersionUtils.IsNetCore)
+            {
+                return Assembly.LoadFrom(assemblyPath);
+            }
+
+            return LoadAssemblyInPsesAlc(assemblyPath);
+        }
+
         private static Func<IDisposable> GetPsesAlcReflectionContextEntryFunc()
         {
-            Type alcType = Type.GetType("System.Runtime.Loader.AssemblyLoadContext");
-            MethodInfo getAlcMethod = alcType.GetMethod("GetLoadContext", BindingFlags.Public | BindingFlags.Static);
-            object psesAlc = getAlcMethod.Invoke(obj: null, new object[] { s_psesAsm });
-            MethodInfo enterReflectionContextMethod = alcType.GetMethod("EnterContextualReflection", BindingFlags.Public | BindingFlags.Instance);
+            PsesAssemblyLoadContext.GetType();
+            MethodInfo enterReflectionContextMethod = AsmLoadContextType.GetMethod("EnterContextualReflection", BindingFlags.Public | BindingFlags.Instance);
 
             return Expression.Lambda<Func<IDisposable>>(
                 Expression.Convert(
-                    Expression.Call(Expression.Constant(psesAlc), enterReflectionContextMethod),
+                    Expression.Call(Expression.Constant(PsesAssemblyLoadContext), enterReflectionContextMethod),
                     typeof(IDisposable))).Compile();
+        }
+
+        private static Func<string, Assembly> GetPsesAlcLoadAsmFunc()
+        {
+            MethodInfo loadFromAssemblyPathMethod = AsmLoadContextType.GetMethod("LoadFromAssemblyPath", BindingFlags.Public | BindingFlags.Instance);
+            return (Func<string, Assembly>)loadFromAssemblyPathMethod.CreateDelegate(typeof(Func<string, Assembly>), PsesAssemblyLoadContext);
+        }
+
+        private static object GetPsesAsmLoadContext()
+        {
+            if (!VersionUtils.IsNetCore)
+            {
+                return null;
+            }
+
+            Type alcType = Type.GetType("System.Runtime.Loader.AssemblyLoadContext");
+            MethodInfo getAlcMethod = alcType.GetMethod("GetLoadContext", BindingFlags.Public | BindingFlags.Static);
+            return getAlcMethod.Invoke(obj: null, new object[] { s_psesAsm });
         }
     }
 }
