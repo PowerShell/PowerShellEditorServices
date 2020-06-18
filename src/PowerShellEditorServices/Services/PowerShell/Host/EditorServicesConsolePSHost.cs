95558cc7b3da1d5449944bbb7a8c8e6ef1992f4a
Robert Holt <rjmholt_msft@outlook.com>
2020-06-17 22:18:16 -0700

    Add progress

---
+++ src/PowerShellEditorServices/Services/PowerShell/Host/EditorServicesConsolePSHost.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.Extensions.Logging;
+using Microsoft.PowerShell.EditorServices.Hosting;
+using System;
+using System.Globalization;
+using System.Management.Automation.Host;
+using System.Management.Automation.Runspaces;
+
+namespace Microsoft.PowerShell.EditorServices.Services.PowerShell.Host
+{
+    internal class EditorServicesConsolePSHost : PSHost, IHostSupportsInteractiveSession
+    {
+        private readonly ILogger _logger;
+
+        private readonly HostStartupInfo _hostInfo;
+
+        private Runspace _pushedRunspace;
+
+        public EditorServicesConsolePSHost(
+            ILogger logger,
+            EditorServicesConsolePSHostUserInterface ui,
+            HostStartupInfo hostInfo)
+        {
+            _logger = logger;
+            _hostInfo = hostInfo;
+            _pushedRunspace = null;
+            UI = ui;
+        }
+
+        public override CultureInfo CurrentCulture => CultureInfo.CurrentCulture;
+
+        public override CultureInfo CurrentUICulture => CultureInfo.CurrentUICulture;
+
+        public override Guid InstanceId { get; } = Guid.NewGuid();
+
+        public override string Name => _hostInfo.Name;
+
+        public override PSHostUserInterface UI { get; }
+
+        public override Version Version => _hostInfo.Version;
+
+        public bool IsRunspacePushed { get; private set; }
+
+        public Runspace Runspace { get; private set; }
+
+        public override void EnterNestedPrompt()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void ExitNestedPrompt()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void NotifyBeginApplication()
+        {
+        }
+
+        public override void NotifyEndApplication()
+        {
+        }
+
+        public void PopRunspace()
+        {
+            Runspace = _pushedRunspace;
+            _pushedRunspace = null;
+            IsRunspacePushed = false;
+        }
+
+        public void PushRunspace(Runspace runspace)
+        {
+            _pushedRunspace = Runspace;
+            Runspace = runspace;
+            IsRunspacePushed = true;
+        }
+
+        public override void SetShouldExit(int exitCode)
+        {
+            if (IsRunspacePushed)
+            {
+                PopRunspace();
+            }
+        }
+    }
+}
---
+++ src/PowerShellEditorServices/Services/PowerShell/Utility/ErrorRecordExtensions.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.PowerShell.EditorServices.Utility;
+using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq.Expressions;
+using System.Management.Automation;
+using System.Reflection;
+
+namespace Microsoft.PowerShell.EditorServices.Services.PowerShell.Utility
+{
+    internal static class ErrorRecordExtensions
+    {
+        private static Action<PSObject> s_setWriteStreamProperty = null;
+
+        [SuppressMessage("Performance", "CA1810:Initialize reference type static fields inline", Justification = "cctor needed for version specific initialization")]
+        static ErrorRecordExtensions()
+        {
+            if (VersionUtils.IsPS7OrGreater)
+            {
+                // Used to write ErrorRecords to the Error stream. Using Public and NonPublic because the plan is to make this property
+                // public in 7.0.1
+                PropertyInfo writeStreamProperty = typeof(PSObject).GetProperty("WriteStream", BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public);
+                Type writeStreamType = typeof(PSObject).Assembly.GetType("System.Management.Automation.WriteStreamType");
+                object errorStreamType = Enum.Parse(writeStreamType, "Error");
+
+                var errorObjectParameter = Expression.Parameter(typeof(PSObject));
+
+                s_setWriteStreamProperty = Expression.Lambda<Action<PSObject>>(
+                    Expression.Call(
+                        errorObjectParameter,
+                        writeStreamProperty.GetSetMethod(),
+                        Expression.Constant(errorStreamType)), 
+                    errorObjectParameter)
+                    .Compile();
+            }
+        }
+
+        public static PSObject AsPSObject(this ErrorRecord errorRecord)
+        {
+            var errorObject = PSObject.AsPSObject(errorRecord);
+
+            // Used to write ErrorRecords to the Error stream so they are rendered in the console correctly.
+            if (s_setWriteStreamProperty != null)
+            {
+                s_setWriteStreamProperty(errorObject);
+            }
+            else
+            {
+                var note = new PSNoteProperty("writeErrorStream", true);
+                errorObject.Properties.Add(note);
+            }
+
+            return errorObject;
+        }
+    }
+}
