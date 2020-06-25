e667cdee675c965ccf182b715581de875e12ae4f
Robert Holt <rjmholt_msft@outlook.com>
2020-06-25 15:08:40 -0700

    Reuse conhost for prompts

--- src/PowerShellEditorServices/Services/PowerShell/Host/EditorServicesConsolePSHostUserInterface.cs
+++ src/PowerShellEditorServices/Services/PowerShell/Host/EditorServicesConsolePSHostUserInterface.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Management.Automation;
 using System.Management.Automation.Host;
+using System.Reflection;
 using System.Security;
 using System.Threading;
 
@@ -18,6 +19,8 @@ namespace Microsoft.PowerShell.EditorServices.Services.PowerShell.Host
 
         private readonly PSHostUserInterface _underlyingHostUI;
 
+        private readonly PSHostUserInterface _consoleHostUI;
+
         public EditorServicesConsolePSHostUserInterface(
             ILoggerFactory loggerFactory,
             ConsoleReadLine readLine,
@@ -27,6 +30,12 @@ namespace Microsoft.PowerShell.EditorServices.Services.PowerShell.Host
             _readLine = readLine;
             _underlyingHostUI = underlyingHostUI;
             RawUI = new EditorServicesConsolePSHostRawUserInterface(loggerFactory, underlyingHostUI.RawUI);
+
+            _consoleHostUI = GetConsoleHostUI(_underlyingHostUI);
+            if (_consoleHostUI != null)
+            {
+                SetConsoleHostUIToInteractive(_consoleHostUI);
+            }
         }
 
         public override PSHostRawUserInterface RawUI { get; }
@@ -35,21 +44,41 @@ namespace Microsoft.PowerShell.EditorServices.Services.PowerShell.Host
 
         public override Dictionary<string, PSObject> Prompt(string caption, string message, Collection<FieldDescription> descriptions)
         {
+            if (_consoleHostUI != null)
+            {
+                return _consoleHostUI.Prompt(caption, message, descriptions);
+            }
+
             return _underlyingHostUI.Prompt(caption, message, descriptions);
         }
 
         public override int PromptForChoice(string caption, string message, Collection<ChoiceDescription> choices, int defaultChoice)
         {
+            if (_consoleHostUI != null)
+            {
+                return _consoleHostUI.PromptForChoice(caption, message, choices, defaultChoice);
+            }
+
             return _underlyingHostUI.PromptForChoice(caption, message, choices, defaultChoice);
         }
 
         public override PSCredential PromptForCredential(string caption, string message, string userName, string targetName, PSCredentialTypes allowedCredentialTypes, PSCredentialUIOptions options)
         {
+            if (_consoleHostUI != null)
+            {
+                return _consoleHostUI.PromptForCredential(caption, message, userName, targetName, allowedCredentialTypes, options);
+            }
+
             return _underlyingHostUI.PromptForCredential(caption, message, userName, targetName, allowedCredentialTypes, options);
         }
 
         public override PSCredential PromptForCredential(string caption, string message, string userName, string targetName)
         {
+            if (_consoleHostUI != null)
+            {
+                return _consoleHostUI.PromptForCredential(caption, message, userName, targetName);
+            }
+
             return _underlyingHostUI.PromptForCredential(caption, message, userName, targetName);
         }
 
@@ -98,6 +127,23 @@ namespace Microsoft.PowerShell.EditorServices.Services.PowerShell.Host
         public override void WriteWarningLine(string message)
         {
             _underlyingHostUI.WriteWarningLine(message);
+        }
+
+        private PSHostUserInterface GetConsoleHostUI(PSHostUserInterface ui)
+        {
+            FieldInfo externalUIField = ui.GetType().GetField("_externalUI", BindingFlags.NonPublic | BindingFlags.Instance);
+
+            if (externalUIField == null)
+            {
+                return null;
+            }
+
+            return (PSHostUserInterface)externalUIField.GetValue(ui);
+        }
+
+        private void SetConsoleHostUIToInteractive(PSHostUserInterface ui)
+        {
+            ui.GetType().GetProperty("ThrowOnReadAndPrompt", BindingFlags.NonPublic | BindingFlags.Instance)?.SetValue(ui, false);
         }
     }
 }
