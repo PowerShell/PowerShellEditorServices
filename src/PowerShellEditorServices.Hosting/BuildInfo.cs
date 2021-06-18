bce339670221929bbf34570fe40eec1db2700047
Darren Kattan <1424395+dkattan@users.noreply.github.com>
2021-06-18 09:23:13 -0500

    Undoing changes to BuildInfo.cs

--- src/PowerShellEditorServices.Hosting/BuildInfo.cs
+++ src/PowerShellEditorServices.Hosting/BuildInfo.cs
@@ -4,9 +4,9 @@ namespace Microsoft.PowerShell.EditorServices.Hosting
 {
     public static class BuildInfo
     {
-        // TODO: Include a Git commit hash in this.
         public static readonly string BuildVersion = "<development-build>";
         public static readonly string BuildOrigin = "<development>";
-        public static readonly System.DateTime? BuildTime = System.DateTime.Parse("2020-05-03T16:39:35", CultureInfo.InvariantCulture.DateTimeFormat);
+        public static readonly string BuildCommit = "<development>";
+        public static readonly System.DateTime? BuildTime = System.DateTime.Parse("2019-12-06T21:43:41", CultureInfo.InvariantCulture.DateTimeFormat);
     }
 }
