96540d50a03a6c5adac3dc8fcaf81430223f73f9
dkattan <1424395+dkattan@users.noreply.github.com>
2021-06-30 17:40:54 -0500

    Adjusted LSPTestsFixtures.cs to enable all client capabilities to fix MethodNotSupportedException in CI tests.

--- test/PowerShellEditorServices.Test.E2E/LSPTestsFixures.cs
+++ test/PowerShellEditorServices.Test.E2E/LSPTestsFixures.cs
@@ -20,6 +20,7 @@ using OmniSharp.Extensions.LanguageServer.Protocol.Document;
 using OmniSharp.Extensions.LanguageServer.Protocol.Models;
 using OmniSharp.Extensions.LanguageServer.Protocol.Window;
 using OmniSharp.Extensions.LanguageServer.Protocol.Workspace;
+using OmniSharp.Extensions.LanguageProtocol.Testing;
 using Xunit;
 using Xunit.Abstractions;
 
@@ -56,6 +57,7 @@ namespace PowerShellEditorServices.Test.E2E
                     .WithInput(_psesProcess.OutputStream)
                     .WithOutput(_psesProcess.InputStream)
                     .WithRootUri(DocumentUri.FromFileSystemPath(testdir.FullName))
+                    .EnableDynamicRegistration().EnableAllCapabilities()
                     .OnPublishDiagnostics(diagnosticParams => Diagnostics.AddRange(diagnosticParams.Diagnostics.Where(d => d != null)))
                     .OnLogMessage(logMessageParams => Output?.WriteLine($"{logMessageParams.Type.ToString()}: {logMessageParams.Message}"))
                     .OnTelemetryEvent(telemetryEventParams => TelemetryEvents.Add(
@@ -64,15 +66,6 @@ namespace PowerShellEditorServices.Test.E2E
                             EventName = (string)telemetryEventParams.ExtensionData["eventName"],
                             Data = telemetryEventParams.ExtensionData["data"] as JObject
                         }));
-
-                // Enable all capabilities this this is for testing.
-                // This will be a built in feature of the Omnisharp client at some point.
-                var capabilityTypes = typeof(ICapability).Assembly.GetExportedTypes()
-                    .Where(z => typeof(ICapability).IsAssignableFrom(z) && z.IsClass && !z.IsAbstract);
-                foreach (Type capabilityType in capabilityTypes)
-                {
-                    options.WithCapability(Activator.CreateInstance(capabilityType, Array.Empty<object>()) as ICapability);
-                }
             });
 
             await PsesLanguageClient.Initialize(CancellationToken.None).ConfigureAwait(false);
