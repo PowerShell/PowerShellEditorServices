8cf570b68836ea04d3869fd072ac57a08dbb713b
Andrew Schwartzmeyer <andrew@schwartzmeyer.com>
2021-07-26 12:58:29 -0700

    Force installation of required modules in `azurePipelinesBuild.ps1`
    This suddenly decided to be an issue, where the modules were being not
    being installed because `-Force` was missing (also clean up because we
    don't need `-Confirm:$false`).

--- tools/azurePipelinesBuild.ps1
+++ tools/azurePipelinesBuild.ps1
@@ -6,9 +6,9 @@ $ErrorActionPreference = 'Stop'
 Set-PSRepository -Name PSGallery -InstallationPolicy Trusted | Out-Null
 if ($IsWindows -or $PSVersionTable.PSVersion.Major -lt 6) {
     # We rely on PowerShellGet's -AllowPrerelease which is in PowerShellGet 1.6 so we need to update PowerShellGet.
-    Get-Module PowerShellGet,PackageManagement | Remove-Module -Force -Verbose
-    powershell -Command { Install-Module -Name PowerShellGet -MinimumVersion 1.6 -Force -Confirm:$false -Verbose }
-    powershell -Command { Install-Module -Name PackageManagement -MinimumVersion 1.1.7.0 -Force -Confirm:$false -Verbose }
+    Get-Module PowerShellGet,PackageManagement | Remove-Module -Force
+    powershell -Command { Install-Module -Name PowerShellGet -MinimumVersion 1.6 -Force }
+    powershell -Command { Install-Module -Name PackageManagement -MinimumVersion 1.1.7.0 -Force }
     Import-Module -Name PowerShellGet -MinimumVersion 1.6 -Force
     Import-Module -Name PackageManagement -MinimumVersion 1.1.7.0 -Force
 }
@@ -17,7 +17,7 @@ if ($IsWindows -or $PSVersionTable.PSVersion.Major -lt 6) {
 Update-Help -Force -ErrorAction SilentlyContinue
 
 # Needed for build and docs gen.
-Install-Module InvokeBuild -MaximumVersion 5.1.0 -Scope CurrentUser
-Install-Module PlatyPS -RequiredVersion 0.9.0 -Scope CurrentUser
+Install-Module -Name InvokeBuild -MaximumVersion 5.1.0 -Scope CurrentUser -Force
+Install-Module -Name PlatyPS -RequiredVersion 0.9.0 -Scope CurrentUser -Force
 
 Invoke-Build -Configuration Release
