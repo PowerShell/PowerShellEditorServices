a77c21f87e17f996eb2c1c7d0d0d4cd2145de997
Robert Holt <rjmholt_msft@outlook.com>
2021-09-02 11:19:52 -0700

    Enable proper debug UI interaction

--- src/PowerShellEditorServices/Services/PowerShell/Debugging/DebuggerResumingEventArgs.cs
+++ src/PowerShellEditorServices/Services/PowerShell/Debugging/DebuggerResumingEventArgs.cs
@@ -2,14 +2,6 @@
 
 namespace Microsoft.PowerShell.EditorServices.Services.PowerShell.Debugging
 {
-    internal class DebuggerResumingEventArgs
-    {
-        public DebuggerResumingEventArgs(DebuggerResumeAction resumeAction)
-        {
-            ResumeAction = resumeAction;
-        }
-
-        public DebuggerResumeAction ResumeAction { get; }
-
-    }
+    internal record DebuggerResumingEventArgs(
+        DebuggerResumeAction ResumeAction);
 }
