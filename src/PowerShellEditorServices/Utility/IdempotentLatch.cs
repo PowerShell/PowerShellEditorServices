e4769755840d43ee452219287b16c6e9cd8becf4
Robert Holt <rjmholt_msft@outlook.com>
2021-09-15 09:51:30 -0700

    Add data structure for interlocked latch pattern

---
+++ src/PowerShellEditorServices/Utility/IdempotentLatch.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace Microsoft.PowerShell.EditorServices.Utility
+{
+    internal class IdempotentLatch
+    {
+        private int _signaled;
+
+        public IdempotentLatch()
+        {
+            _signaled = 0;
+        }
+
+        public bool IsSignaled => _signaled != 0;
+
+        public bool TryEnter() => Interlocked.Exchange(ref _signaled, 1) == 0;
+    }
+}
