543dabe0646fc56d589c15c6d385c03b07da170c
Robert Holt <rjmholt_msft@outlook.com>
2021-10-08 17:09:07 -0700

    Fix Ctrl-C detection

--- src/PowerShellEditorServices/Services/PowerShell/Host/PsesInternalHost.cs
+++ src/PowerShellEditorServices/Services/PowerShell/Host/PsesInternalHost.cs
@@ -695,7 +695,7 @@ namespace Microsoft.PowerShell.EditorServices.Services.PowerShell.Host
             return _lastKey.HasValue
                 && _lastKey.Value.Key == ConsoleKey.C
                 && (_lastKey.Value.Modifiers & ConsoleModifiers.Control) != 0
-                && (_lastKey.Value.Modifiers & ConsoleModifiers.Alt) != 0;
+                && (_lastKey.Value.Modifiers & ConsoleModifiers.Alt) == 0;
         }
 
         private void OnDebuggerStopped(object sender, DebuggerStopEventArgs debuggerStopEventArgs)
