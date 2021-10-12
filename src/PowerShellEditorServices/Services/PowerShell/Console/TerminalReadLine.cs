7ab86391e037e93ca4fe91ee857d333b92a976a5
Robert Holt <rjmholt_msft@outlook.com>
2021-10-12 16:21:23 -0700

    Add common secure string read functionality

---
+++ src/PowerShellEditorServices/Services/PowerShell/Console/TerminalReadLine.cs
@@ -0,0 +1,114 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using System.Security;
+using System.Threading;
+
+namespace Microsoft.PowerShell.EditorServices.Services.PowerShell.Console
+{
+    using System;
+    using System.Management.Automation;
+
+    internal abstract class TerminalReadLine : IReadLine
+    {
+        public abstract string ReadLine(CancellationToken cancellationToken);
+
+        public abstract bool TryOverrideIdleHandler(Action<CancellationToken> idleHandler);
+
+        public abstract bool TryOverrideReadKey(Func<bool, ConsoleKeyInfo> readKeyOverride);
+
+        protected abstract ConsoleKeyInfo ReadKey(CancellationToken cancellationToken);
+
+        public SecureString ReadSecureLine(CancellationToken cancellationToken)
+        {
+            Console.TreatControlCAsInput = true;
+            int previousInputLength = 0;
+            SecureString secureString = new SecureString();
+            try
+            {
+                bool enterPressed = false;
+                while (!enterPressed && !cancellationToken.IsCancellationRequested)
+                {
+                    ConsoleKeyInfo keyInfo = ReadKey(cancellationToken);
+
+                    if (IsCtrlC(keyInfo))
+                    {
+                        throw new PipelineStoppedException();
+                    }
+
+                    switch (keyInfo.Key)
+                    {
+                        case ConsoleKey.Enter:
+                            // Break to return the completed string
+                            enterPressed = true;
+                            continue;
+
+                        case ConsoleKey.Tab:
+                            break;
+
+                        case ConsoleKey.Backspace:
+                            if (secureString.Length > 0)
+                            {
+                                secureString.RemoveAt(secureString.Length - 1);
+                            }
+                            break;
+
+                        default:
+                            if (keyInfo.KeyChar != 0 && !char.IsControl(keyInfo.KeyChar))
+                            {
+                                secureString.AppendChar(keyInfo.KeyChar);
+                            }
+                            break;
+                    }
+
+                    // Re-render the secure string characters
+                    int currentInputLength = secureString.Length;
+                    int consoleWidth = Console.WindowWidth;
+
+                    if (currentInputLength > previousInputLength)
+                    {
+                        Console.Write('*');
+                    }
+                    else if (previousInputLength > 0 && currentInputLength < previousInputLength)
+                    {
+                        int row = ConsoleProxy.GetCursorTop(cancellationToken);
+                        int col = ConsoleProxy.GetCursorLeft(cancellationToken);
+
+                        // Back up the cursor before clearing the character
+                        col--;
+                        if (col < 0)
+                        {
+                            col = consoleWidth - 1;
+                            row--;
+                        }
+
+                        Console.SetCursorPosition(col, row);
+                        Console.Write(' ');
+                        Console.SetCursorPosition(col, row);
+                    }
+
+                    previousInputLength = currentInputLength;
+                }
+            }
+            finally
+            {
+                Console.TreatControlCAsInput = false;
+            }
+
+            return secureString;
+        }
+
+        protected static bool IsCtrlC(ConsoleKeyInfo keyInfo)
+        {
+            if ((int)keyInfo.Key == 3)
+            {
+                return true;
+            }
+
+            return keyInfo.Key == ConsoleKey.C
+                && (keyInfo.Modifiers & ConsoleModifiers.Control) != 0
+                && (keyInfo.Modifiers & ConsoleModifiers.Alt) == 0;
+        }
+
+    }
+}
