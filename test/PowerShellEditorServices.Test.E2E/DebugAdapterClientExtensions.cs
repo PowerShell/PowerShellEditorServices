4184362995acea6bd2de26264c8f4986882e8e66
Andrew Schwartzmeyer <andrew@schwartzmeyer.com>
2022-02-01 16:06:16 -0800

    Clean up LSP message tests

--- test/PowerShellEditorServices.Test.E2E/DebugAdapterClientExtensions.cs
+++ test/PowerShellEditorServices.Test.E2E/DebugAdapterClientExtensions.cs
@@ -14,23 +14,24 @@ namespace PowerShellEditorServices.Test.E2E
     {
         public static async Task LaunchScript(this DebugAdapterClient debugAdapterClient, string filePath, TaskCompletionSource<object> started)
         {
-            LaunchResponse launchResponse = await debugAdapterClient.Launch(new PsesLaunchRequestArguments
-            {
-                NoDebug = false,
-                Script = filePath,
-                Cwd = "",
-                CreateTemporaryIntegratedConsole = false,
-            }).ConfigureAwait(false);
+            LaunchResponse launchResponse = await debugAdapterClient.Launch(
+                new PsesLaunchRequestArguments
+                {
+                    NoDebug = false,
+                    Script = filePath,
+                    Cwd = "",
+                    CreateTemporaryIntegratedConsole = false
+                }).ConfigureAwait(true);
 
-            if (launchResponse == null)
+            if (launchResponse is null)
             {
                 throw new Exception("Launch response was null.");
             }
 
             // This will check to see if we received the Initialized event from the server.
             await Task.Run(
-                async () => await started.Task.ConfigureAwait(false),
-                new CancellationTokenSource(2000).Token).ConfigureAwait(false);
+                async () => await started.Task.ConfigureAwait(true),
+                new CancellationTokenSource(2000).Token).ConfigureAwait(true);
         }
     }
 }
