ac1b1c8d4c81885cfdc8daeeb81564c7c5f117e6
Razmo99 <3089087+Razmo99@users.noreply.github.com>
2023-03-29 20:55:25 +1100

    Adding in unit teste for refactoring functions

---
+++ test/PowerShellEditorServices.Test.Shared/Refactoring/RefactorsFunctionData.cs
@@ -0,0 +1,42 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+using Microsoft.PowerShell.EditorServices.Handlers;
+
+namespace PowerShellEditorServices.Test.Shared.Refactoring
+{
+    internal static class RefactorsFunctionData
+    {
+        public static readonly RenameSymbolParams FunctionsMultiple = new()
+        {
+            // rename function Two { ...}
+            FileName = "FunctionsMultiple.ps1",
+            Column = 9,
+            Line = 3,
+            RenameTo = "TwoFours"
+        };
+        public static readonly RenameSymbolParams FunctionsMultipleFromCommandDef = new()
+        {
+            //  ... write-host "Three Hello" ...
+            // Two
+            //
+            FileName = "FunctionsMultiple.ps1",
+            Column = 5,
+            Line = 15,
+            RenameTo = "OnePlusOne"
+        };
+        public static readonly RenameSymbolParams FunctionsSingleParams = new()
+        {
+            FileName = "FunctionsSingle.ps1",
+            Column = 9,
+            Line = 0,
+            RenameTo = "OneMethod"
+        };
+                public static readonly RenameSymbolParams FunctionsSingleNested = new()
+        {
+            FileName = "FunctionsNestedSimple.ps1",
+            Column = 16,
+            Line = 4,
+            RenameTo = "OneMethod"
+        };
+    }
+}
