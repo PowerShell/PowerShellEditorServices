b22e7ef6798939fea13087cfd4e9dd9842071c64
Razmo99 <3089087+Razmo99@users.noreply.github.com>
2023-10-14 21:06:16 +0300

    adjusting tests for more complexity

--- test/PowerShellEditorServices.Test.Shared/Refactoring/Variables/VariableParameterCommndWithSameNameRenamed.ps1
+++ test/PowerShellEditorServices.Test.Shared/Refactoring/Variables/VariableParameterCommndWithSameNameRenamed.ps1
@@ -15,6 +15,40 @@ function Test-AADConnected {
     }
 }
 
+function Set-MSolUMFA{
+    [CmdletBinding(SupportsShouldProcess=$true)]
+    param (
+        [Parameter(Mandatory=$true,ValueFromPipelineByPropertyName=$true)][string]$UserPrincipalName,
+        [Parameter(Mandatory=$true,ValueFromPipelineByPropertyName=$true)][ValidateSet('Enabled','Disabled','Enforced')][String]$StrongAuthenticationRequiremets
+    )
+    begin{
+        # Check if connected to Msol Session already
+        if (!(Test-MSolConnected)) {
+            Write-Verbose('No existing Msol session detected')
+            try {
+                Write-Verbose('Initiating connection to Msol')
+                Connect-MsolService -ErrorAction Stop
+                Write-Verbose('Connected to Msol successfully')
+            }catch{
+                return Write-Error($_.Exception.Message)
+            }
+        }
+        if(!(Get-MsolUser -MaxResults 1 -ErrorAction Stop)){
+            return Write-Error('Insufficient permissions to set MFA')
+        }
+    }
+    Process{
+        # Get the time and calc 2 min to the future
+        $TimeStart = Get-Date
+        $TimeEnd = $timeStart.addminutes(1)
+        $Finished=$false
+        #Loop to check if the user exists already
+        if ($PSCmdlet.ShouldProcess($UserPrincipalName, "StrongAuthenticationRequiremets = "+$StrongAuthenticationRequiremets)) {
+        }
+    }
+    End{}
+}
+
 Set-MsolUser -UserPrincipalName $UPN -StrongAuthenticationRequirements $sta -ErrorAction Stop
 $UserPrincipalName = "Bob"
 if ($UserPrincipalName) {
