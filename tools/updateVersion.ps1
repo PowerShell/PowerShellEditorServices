20f90a9d28bb181acc3137e4eeba8625399f4500
Andy Jordan <2226434+andyleejordan@users.noreply.github.com>
2024-04-01 15:39:16 -0700

    Support 'preview' in version and always publish (a draft)
    Removed the manual validation step because it is unnecesary (and annoying)
    to gate this pipeline since it opens an unpublished, draft release.
    The downstream triggered pipeline will gate the release to the VS Code
    marketplace.

--- tools/updateVersion.ps1
+++ tools/updateVersion.ps1
@@ -16,30 +16,32 @@ if ($LASTEXITCODE -ne 0) {
 
 $v = "$($Version.Major).$($Version.Minor).$($Version.Patch)"
 
-$path = "PowerShellEditorServices.Common.props"
-$f = Get-Content -Path $path
+$Path = "PowerShellEditorServices.Common.props"
+$f = Get-Content -Path $Path
 $f = $f -replace '^(?<prefix>\s+<VersionPrefix>)(.+)(?<suffix></VersionPrefix>)$', "`${prefix}${v}`${suffix}"
 $f = $f -replace '^(?<prefix>\s+<VersionSuffix>)(.*)(?<suffix></VersionSuffix>)$', "`${prefix}$($Version.PreReleaseLabel)`${suffix}"
-$f | Set-Content -Path $path
-git add $path
+$f | Set-Content -Path $Path
+git add $Path
 
-$path = "module/PowerShellEditorServices/PowerShellEditorServices.psd1"
-$f = Get-Content -Path $path
+$Path = "module/PowerShellEditorServices/PowerShellEditorServices.psd1"
+$f = Get-Content -Path $Path
 $f = $f -replace "^(?<prefix>ModuleVersion = ')(.+)(?<suffix>')`$", "`${prefix}${v}`${suffix}"
-$f | Set-Content -Path $path
-git add $path
+$f | Set-Content -Path $Path
+git add $Path
 
-$path = "CHANGELOG.md"
-$Changelog = Get-Content -Path $path
+$Path = "CHANGELOG.md"
+$Changelog = Get-Content -Path $Path
 @(
     $Changelog[0..1]
     "## v$Version"
     "### $([datetime]::Now.ToString('dddd, MMMM dd, yyyy'))"
     ""
+    "See more details at the GitHub Release for [v$Version](https://github.com/PowerShell/PowerShellEditorServices/releases/tag/v$Version)."
+    ""
     $Changes
     ""
     $Changelog[2..$Changelog.Length]
-) | Set-Content -Encoding utf8NoBOM -Path $path
-git add $path
+) | Set-Content -Encoding utf8NoBOM -Path $Path
+git add $Path
 
 git commit --edit --message "v$($Version): $Changes"
