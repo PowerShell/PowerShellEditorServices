8f266bd022886f6d77c18ca85b76e46090deb81a
Razmo99 <3089087+Razmo99@users.noreply.github.com>
2024-06-07 13:35:19 +1000

    moved serializer and getmodifiedscriptcontent seperate file as it will be reused for variable tests

--- test/PowerShellEditorServices.Test/Refactoring/RefactorFunctionTests.cs
+++ test/PowerShellEditorServices.Test/Refactoring/RefactorFunctionTests.cs
@@ -1,7 +1,6 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-using System;
 using System.IO;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Logging.Abstractions;
@@ -15,8 +14,7 @@ using Xunit;
 using Microsoft.PowerShell.EditorServices.Services.Symbols;
 using Microsoft.PowerShell.EditorServices.Refactoring;
 using PowerShellEditorServices.Test.Shared.Refactoring.Functions;
-using Xunit.Abstractions;
-using MediatR;
+using static PowerShellEditorServices.Test.Refactoring.RefactorUtilities;
 
 namespace PowerShellEditorServices.Test.Refactoring
 {
@@ -36,24 +34,6 @@ namespace PowerShellEditorServices.Test.Refactoring
         public async Task DisposeAsync() => await Task.Run(psesHost.StopAsync);
         private ScriptFile GetTestScript(string fileName) => workspace.GetFile(TestUtilities.GetSharedPath(Path.Combine("Refactoring", "Functions", fileName)));
 
-        internal static string GetModifiedScript(string OriginalScript, ModifiedFileResponse Modification)
-        {
-
-            string[] Lines = OriginalScript.Split(
-                            new string[] { Environment.NewLine },
-                            StringSplitOptions.None);
-
-            foreach (TextChange change in Modification.Changes)
-            {
-                string TargetLine = Lines[change.StartLine];
-                string begin = TargetLine.Substring(0, change.StartColumn);
-                string end = TargetLine.Substring(change.EndColumn);
-                Lines[change.StartLine] = begin + change.NewText + end;
-            }
-
-            return string.Join(Environment.NewLine, Lines);
-        }
-
         internal static string TestRenaming(ScriptFile scriptFile, RenameSymbolParamsSerialized request, SymbolReference symbol)
         {
             IterativeFunctionRename iterative = new(symbol.NameRegion.Text,
@@ -67,44 +47,6 @@ namespace PowerShellEditorServices.Test.Refactoring
                 Changes = iterative.Modifications
             };
             return GetModifiedScript(scriptFile.Contents, changes);
-        }
-
-        public class RenameSymbolParamsSerialized : IRequest<RenameSymbolResult>, IXunitSerializable
-        {
-            public string FileName { get; set; }
-            public int Line { get; set; }
-            public int Column { get; set; }
-            public string RenameTo { get; set; }
-
-            // Default constructor needed for deserialization
-            public RenameSymbolParamsSerialized() { }
-
-            // Parameterized constructor for convenience
-            public RenameSymbolParamsSerialized(RenameSymbolParams RenameSymbolParams)
-            {
-                FileName = RenameSymbolParams.FileName;
-                Line = RenameSymbolParams.Line;
-                Column = RenameSymbolParams.Column;
-                RenameTo = RenameSymbolParams.RenameTo;
-            }
-
-            public void Deserialize(IXunitSerializationInfo info)
-            {
-                FileName = info.GetValue<string>("FileName");
-                Line = info.GetValue<int>("Line");
-                Column = info.GetValue<int>("Column");
-                RenameTo = info.GetValue<string>("RenameTo");
-            }
-
-            public void Serialize(IXunitSerializationInfo info)
-            {
-                info.AddValue("FileName", FileName);
-                info.AddValue("Line", Line);
-                info.AddValue("Column", Column);
-                info.AddValue("RenameTo", RenameTo);
-            }
-
-            public override string ToString() => $"{FileName}";
         }
 
         public class FunctionRenameTestData : TheoryData<RenameSymbolParamsSerialized>
