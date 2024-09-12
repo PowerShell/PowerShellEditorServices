70af86337c809f72e400debb3cfc4717b10b2072
Justin Grote <JustinGrote@users.noreply.github.com>
2024-09-12 02:41:05 -0700

    Move all Handling to OmniSharp LSP types

--- src/PowerShellEditorServices/Services/PowerShell/Handlers/PrepareRenameSymbol.cs
+++ src/PowerShellEditorServices/Services/PowerShell/Handlers/PrepareRenameSymbol.cs
@@ -3,121 +3,108 @@
 
 using System.Threading;
 using System.Threading.Tasks;
-using MediatR;
 using System.Management.Automation.Language;
-using OmniSharp.Extensions.JsonRpc;
 using Microsoft.PowerShell.EditorServices.Services;
 using Microsoft.PowerShell.EditorServices.Services.TextDocument;
 using Microsoft.PowerShell.EditorServices.Refactoring;
 using Microsoft.PowerShell.EditorServices.Services.Symbols;
+using OmniSharp.Extensions.LanguageServer.Protocol.Models;
+using OmniSharp.Extensions.LanguageServer.Protocol.Document;
+using OmniSharp.Extensions.LanguageServer.Protocol.Client.Capabilities;
 
-namespace Microsoft.PowerShell.EditorServices.Handlers
+namespace Microsoft.PowerShell.EditorServices.Handlers;
+
+internal class PrepareRenameHandler(WorkspaceService workspaceService) : IPrepareRenameHandler
 {
-    [Serial, Method("powerShell/PrepareRenameSymbol")]
-    internal interface IPrepareRenameSymbolHandler : IJsonRpcRequestHandler<PrepareRenameSymbolParams, PrepareRenameSymbolResult> { }
+    public RenameRegistrationOptions GetRegistrationOptions(RenameCapability capability, ClientCapabilities clientCapabilities) => capability.PrepareSupport ? new() { PrepareProvider = true } : new();
 
-    internal class PrepareRenameSymbolParams : IRequest<PrepareRenameSymbolResult>
+    public async Task<RangeOrPlaceholderRange> Handle(PrepareRenameParams request, CancellationToken cancellationToken)
     {
-        public string FileName { get; set; }
-        public int Line { get; set; }
-        public int Column { get; set; }
-        public string RenameTo { get; set; }
-    }
-    internal class PrepareRenameSymbolResult
-    {
-        public string message;
-    }
-
-    internal class PrepareRenameSymbolHandler : IPrepareRenameSymbolHandler
-    {
-        private readonly WorkspaceService _workspaceService;
-
-        public PrepareRenameSymbolHandler(WorkspaceService workspaceService) => _workspaceService = workspaceService;
-
-        public async Task<PrepareRenameSymbolResult> Handle(PrepareRenameSymbolParams request, CancellationToken cancellationToken)
+        ScriptFile scriptFile = workspaceService.GetFile(request.TextDocument.Uri);
+        if (Utilities.AssertContainsDotSourced(scriptFile.ScriptAst))
         {
-            if (!_workspaceService.TryGetFile(request.FileName, out ScriptFile scriptFile))
-            {
-                // TODO: Unsaved file support. We need to find the unsaved file in the text documents synced to the LSP and use that as our Ast Base.
-                throw new HandlerErrorException($"File {request.FileName} not found in workspace. Unsaved files currently do not support the rename symbol feature.");
-            }
-            return await Task.Run(() =>
-            {
-                PrepareRenameSymbolResult result = new()
-                {
-                    message = ""
-                };
-                // ast is FunctionDefinitionAst or CommandAst or VariableExpressionAst or StringConstantExpressionAst &&
-                SymbolReference symbol = scriptFile.References.TryGetSymbolAtPosition(request.Line + 1, request.Column + 1);
-                Ast token = Utilities.GetAst(request.Line + 1, request.Column + 1, scriptFile.ScriptAst);
-
-                if (token == null)
-                {
-                    result.message = "Unable to find symbol";
-                    return result;
-                }
-                if (Utilities.AssertContainsDotSourced(scriptFile.ScriptAst))
-                {
-                    result.message = "Dot Source detected, this is currently not supported operation aborted";
-                    return result;
-                }
-
-                bool IsFunction = false;
-                string tokenName = "";
-
-                switch (token)
-                {
-
-                    case FunctionDefinitionAst FuncAst:
-                        IsFunction = true;
-                        tokenName = FuncAst.Name;
-                        break;
-                    case VariableExpressionAst or CommandParameterAst or ParameterAst:
-                        IsFunction = false;
-                        tokenName = request.RenameTo;
-                        break;
-                    case StringConstantExpressionAst:
-
-                        if (token.Parent is CommandAst CommAst)
-                        {
-                            IsFunction = true;
-                            tokenName = CommAst.GetCommandName();
-                        }
-                        else
-                        {
-                            IsFunction = false;
-                        }
-                        break;
-                }
-
-                if (IsFunction)
-                {
-                    try
-                    {
-                        IterativeFunctionRename visitor = new(tokenName,
-                            request.RenameTo,
-                            token.Extent.StartLineNumber,
-                            token.Extent.StartColumnNumber,
-                            scriptFile.ScriptAst);
-                    }
-                    catch (FunctionDefinitionNotFoundException)
-                    {
-                        result.message = "Failed to Find function definition within current file";
-                    }
-                }
-                else
-                {
-                    IterativeVariableRename visitor = new(tokenName,
-                                        token.Extent.StartLineNumber,
-                                        token.Extent.StartColumnNumber,
-                                        scriptFile.ScriptAst);
-                    if (visitor.TargetVariableAst == null)
-                    {
-                        result.message = "Failed to find variable definition within the current file";
-                    }
-                }
-                return result;
-            }).ConfigureAwait(false);
+            throw new HandlerErrorException("Dot Source detected, this is currently not supported");
         }
+
+        int line = request.Position.Line;
+        int column = request.Position.Character;
+        SymbolReference symbol = scriptFile.References.TryGetSymbolAtPosition(line, column);
+
+        if (symbol == null)
+        {
+            return null;
+        }
+
+        RangeOrPlaceholderRange symbolRange = new(symbol.NameRegion.ToRange());
+
+        Ast token = Utilities.GetAst(line, column, scriptFile.ScriptAst);
+
+        return token switch
+        {
+            FunctionDefinitionAst => symbolRange,
+            VariableExpressionAst => symbolRange,
+            CommandParameterAst => symbolRange,
+            ParameterAst => symbolRange,
+            StringConstantExpressionAst stringConstAst when stringConstAst.Parent is CommandAst => symbolRange,
+            _ => null,
+        };
+
+        // TODO: Reimplement the more specific rename criteria (variables and functions only)
+
+        //     bool IsFunction = false;
+        //     string tokenName = "";
+
+        //     switch (token)
+        //     {
+        //         case FunctionDefinitionAst FuncAst:
+        //             IsFunction = true;
+        //             tokenName = FuncAst.Name;
+        //             break;
+        //         case VariableExpressionAst or CommandParameterAst or ParameterAst:
+        //             IsFunction = false;
+        //             tokenName = request.RenameTo;
+        //             break;
+        //         case StringConstantExpressionAst:
+
+        //             if (token.Parent is CommandAst CommAst)
+        //             {
+        //                 IsFunction = true;
+        //                 tokenName = CommAst.GetCommandName();
+        //             }
+        //             else
+        //             {
+        //                 IsFunction = false;
+        //             }
+        //             break;
+        //     }
+
+        //     if (IsFunction)
+        //     {
+        //         try
+        //         {
+        //             IterativeFunctionRename visitor = new(tokenName,
+        //                 request.RenameTo,
+        //                 token.Extent.StartLineNumber,
+        //                 token.Extent.StartColumnNumber,
+        //                 scriptFile.ScriptAst);
+        //         }
+        //         catch (FunctionDefinitionNotFoundException)
+        //         {
+        //             result.message = "Failed to Find function definition within current file";
+        //         }
+        //     }
+        //     else
+        //     {
+        //         IterativeVariableRename visitor = new(tokenName,
+        //                             token.Extent.StartLineNumber,
+        //                             token.Extent.StartColumnNumber,
+        //                             scriptFile.ScriptAst);
+        //         if (visitor.TargetVariableAst == null)
+        //         {
+        //             result.message = "Failed to find variable definition within the current file";
+        //         }
+        //     }
+        //     return result;
+        // }).ConfigureAwait(false);
     }
 }
--- src/PowerShellEditorServices/Services/PowerShell/Handlers/RenameSymbol.cs
+++ src/PowerShellEditorServices/Services/PowerShell/Handlers/RenameSymbol.cs
@@ -6,142 +6,205 @@ using System.Threading;
 using System.Threading.Tasks;
 using MediatR;
 using System.Management.Automation.Language;
-using OmniSharp.Extensions.JsonRpc;
 using Microsoft.PowerShell.EditorServices.Services;
 using Microsoft.PowerShell.EditorServices.Services.TextDocument;
 using Microsoft.PowerShell.EditorServices.Refactoring;
-using System;
-namespace Microsoft.PowerShell.EditorServices.Handlers
+using OmniSharp.Extensions.LanguageServer.Protocol.Document;
+using OmniSharp.Extensions.LanguageServer.Protocol.Models;
+using OmniSharp.Extensions.LanguageServer.Protocol.Client.Capabilities;
+using System.Linq;
+using OmniSharp.Extensions.LanguageServer.Protocol;
+namespace Microsoft.PowerShell.EditorServices.Handlers;
+
+internal class RenameHandler(WorkspaceService workspaceService) : IRenameHandler
 {
-    [Serial, Method("powerShell/renameSymbol")]
-    internal interface IRenameSymbolHandler : IJsonRpcRequestHandler<RenameSymbolParams, RenameSymbolResult> { }
+    // RenameOptions may only be specified if the client states that it supports prepareSupport in its initial initialize request.
+    public RenameRegistrationOptions GetRegistrationOptions(RenameCapability capability, ClientCapabilities clientCapabilities) => capability.PrepareSupport ? new() { PrepareProvider = true } : new();
 
-    public class RenameSymbolOptions
-    {
-        public bool CreateAlias { get; set; }
-    }
 
-    public class RenameSymbolParams : IRequest<RenameSymbolResult>
+    public async Task<WorkspaceEdit> Handle(RenameParams request, CancellationToken cancellationToken)
     {
-        public string FileName { get; set; }
-        public int Line { get; set; }
-        public int Column { get; set; }
-        public string RenameTo { get; set; }
-        public RenameSymbolOptions Options { get; set; }
-    }
-    public class TextChange
-    {
-        public string NewText { get; set; }
-        public int StartLine { get; set; }
-        public int StartColumn { get; set; }
-        public int EndLine { get; set; }
-        public int EndColumn { get; set; }
-    }
-    public class ModifiedFileResponse
-    {
-        public string FileName { get; set; }
-        public List<TextChange> Changes { get; set; }
-        public ModifiedFileResponse(string fileName)
+        ScriptFile scriptFile = workspaceService.GetFile(request.TextDocument.Uri);
+
+        // AST counts from 1 whereas LSP counts from 0
+        int line = request.Position.Line + 1;
+        int column = request.Position.Character + 1;
+
+        Ast tokenToRename = Utilities.GetAst(line, column, scriptFile.ScriptAst);
+
+        ModifiedFileResponse changes = tokenToRename switch
         {
-            FileName = fileName;
-            Changes = new List<TextChange>();
+            FunctionDefinitionAst or CommandAst => RenameFunction(tokenToRename, scriptFile.ScriptAst, request),
+            VariableExpressionAst => RenameVariable(tokenToRename, scriptFile.ScriptAst, request),
+            _ => throw new HandlerErrorException("This should not happen as PrepareRename should have already checked for viability. File an issue if you see this.")
+        };
+
+
+        // TODO: Update changes to work directly and not require this adapter
+        TextEdit[] textEdits = changes.Changes.Select(change => new TextEdit
+        {
+            Range = new Range
+            {
+                Start = new Position { Line = change.StartLine, Character = change.StartColumn },
+                End = new Position { Line = change.EndLine, Character = change.EndColumn }
+            },
+            NewText = change.NewText
+        }).ToArray();
+
+        return new WorkspaceEdit
+        {
+            Changes = new Dictionary<DocumentUri, IEnumerable<TextEdit>>
+            {
+                [request.TextDocument.Uri] = textEdits
+            }
+        };
+    }
+
+
+    internal static ModifiedFileResponse RenameFunction(Ast token, Ast scriptAst, RenameParams requestParams)
+    {
+        RenameSymbolParams request = new()
+        {
+            FileName = requestParams.TextDocument.Uri.ToString(),
+            Line = requestParams.Position.Line,
+            Column = requestParams.Position.Character,
+            RenameTo = requestParams.NewName
+        };
+
+        string tokenName = "";
+        if (token is FunctionDefinitionAst funcDef)
+        {
+            tokenName = funcDef.Name;
         }
-
-        public void AddTextChange(Ast Symbol, string NewText)
+        else if (token.Parent is CommandAst CommAst)
         {
-            Changes.Add(
-                new TextChange
-                {
-                    StartColumn = Symbol.Extent.StartColumnNumber - 1,
-                    StartLine = Symbol.Extent.StartLineNumber - 1,
-                    EndColumn = Symbol.Extent.EndColumnNumber - 1,
-                    EndLine = Symbol.Extent.EndLineNumber - 1,
-                    NewText = NewText
-                }
+            tokenName = CommAst.GetCommandName();
+        }
+        IterativeFunctionRename visitor = new(tokenName,
+                    request.RenameTo,
+                    token.Extent.StartLineNumber,
+                    token.Extent.StartColumnNumber,
+                    scriptAst);
+        visitor.Visit(scriptAst);
+        ModifiedFileResponse FileModifications = new(request.FileName)
+        {
+            Changes = visitor.Modifications
+        };
+        return FileModifications;
+    }
+
+    internal static ModifiedFileResponse RenameVariable(Ast symbol, Ast scriptAst, RenameParams requestParams)
+    {
+        RenameSymbolParams request = new()
+        {
+            FileName = requestParams.TextDocument.Uri.ToString(),
+            Line = requestParams.Position.Line,
+            Column = requestParams.Position.Character,
+            RenameTo = requestParams.NewName
+        };
+        if (symbol is VariableExpressionAst or ParameterAst or CommandParameterAst or StringConstantExpressionAst)
+        {
+
+            IterativeVariableRename visitor = new(
+                request.RenameTo,
+                symbol.Extent.StartLineNumber,
+                symbol.Extent.StartColumnNumber,
+                scriptAst,
+                request.Options ?? null
             );
-        }
-    }
-    public class RenameSymbolResult
-    {
-        public RenameSymbolResult() => Changes = new List<ModifiedFileResponse>();
-        public List<ModifiedFileResponse> Changes { get; set; }
-    }
-
-    internal class RenameSymbolHandler : IRenameSymbolHandler
-    {
-        private readonly WorkspaceService _workspaceService;
-
-        public RenameSymbolHandler(WorkspaceService workspaceService) => _workspaceService = workspaceService;
-
-        internal static ModifiedFileResponse RenameFunction(Ast token, Ast scriptAst, RenameSymbolParams request)
-        {
-            string tokenName = "";
-            if (token is FunctionDefinitionAst funcDef)
-            {
-                tokenName = funcDef.Name;
-            }
-            else if (token.Parent is CommandAst CommAst)
-            {
-                tokenName = CommAst.GetCommandName();
-            }
-            IterativeFunctionRename visitor = new(tokenName,
-                        request.RenameTo,
-                        token.Extent.StartLineNumber,
-                        token.Extent.StartColumnNumber,
-                        scriptAst);
             visitor.Visit(scriptAst);
             ModifiedFileResponse FileModifications = new(request.FileName)
             {
                 Changes = visitor.Modifications
             };
             return FileModifications;
+
         }
-
-        internal static ModifiedFileResponse RenameVariable(Ast symbol, Ast scriptAst, RenameSymbolParams request)
-        {
-            if (symbol is VariableExpressionAst or ParameterAst or CommandParameterAst or StringConstantExpressionAst)
-            {
-
-                IterativeVariableRename visitor = new(
-                    request.RenameTo,
-                    symbol.Extent.StartLineNumber,
-                    symbol.Extent.StartColumnNumber,
-                    scriptAst,
-                    request.Options ?? null
-                );
-                visitor.Visit(scriptAst);
-                ModifiedFileResponse FileModifications = new(request.FileName)
-                {
-                    Changes = visitor.Modifications
-                };
-                return FileModifications;
-
-            }
-            return null;
-        }
-
-        public async Task<RenameSymbolResult> Handle(RenameSymbolParams request, CancellationToken cancellationToken)
-        {
-            if (!_workspaceService.TryGetFile(request.FileName, out ScriptFile scriptFile))
-            {
-                throw new InvalidOperationException("This should not happen as PrepareRename should have already checked for viability. File an issue if you see this.");
-            }
-
-            return await Task.Run(() =>
-            {
-                Ast token = Utilities.GetAst(request.Line + 1, request.Column + 1, scriptFile.ScriptAst);
-                if (token == null) { return null; }
-
-                ModifiedFileResponse FileModifications = (token is FunctionDefinitionAst || token.Parent is CommandAst)
-                    ? RenameFunction(token, scriptFile.ScriptAst, request)
-                    : RenameVariable(token, scriptFile.ScriptAst, request);
-
-                RenameSymbolResult result = new();
-
-                result.Changes.Add(FileModifications);
-
-                return result;
-            }).ConfigureAwait(false);
-        }
+        return null;
     }
 }
+
+// {
+//     [Serial, Method("powerShell/renameSymbol")]
+//     internal interface IRenameSymbolHandler : IJsonRpcRequestHandler<RenameSymbolParams, RenameSymbolResult> { }
+
+public class RenameSymbolOptions
+{
+    public bool CreateAlias { get; set; }
+}
+
+public class RenameSymbolParams : IRequest<RenameSymbolResult>
+{
+    public string FileName { get; set; }
+    public int Line { get; set; }
+    public int Column { get; set; }
+    public string RenameTo { get; set; }
+    public RenameSymbolOptions Options { get; set; }
+}
+public class TextChange
+{
+    public string NewText { get; set; }
+    public int StartLine { get; set; }
+    public int StartColumn { get; set; }
+    public int EndLine { get; set; }
+    public int EndColumn { get; set; }
+}
+public class ModifiedFileResponse
+{
+    public string FileName { get; set; }
+    public List<TextChange> Changes { get; set; }
+    public ModifiedFileResponse(string fileName)
+    {
+        FileName = fileName;
+        Changes = new List<TextChange>();
+    }
+
+    public void AddTextChange(Ast Symbol, string NewText)
+    {
+        Changes.Add(
+            new TextChange
+            {
+                StartColumn = Symbol.Extent.StartColumnNumber - 1,
+                StartLine = Symbol.Extent.StartLineNumber - 1,
+                EndColumn = Symbol.Extent.EndColumnNumber - 1,
+                EndLine = Symbol.Extent.EndLineNumber - 1,
+                NewText = NewText
+            }
+        );
+    }
+}
+public class RenameSymbolResult
+{
+    public RenameSymbolResult() => Changes = new List<ModifiedFileResponse>();
+    public List<ModifiedFileResponse> Changes { get; set; }
+}
+
+//     internal class RenameSymbolHandler : IRenameSymbolHandler
+//     {
+//         private readonly WorkspaceService _workspaceService;
+
+//         public RenameSymbolHandler(WorkspaceService workspaceService) => _workspaceService = workspaceService;
+
+
+
+
+//         public async Task<RenameSymbolResult> Handle(RenameSymbolParams request, CancellationToken cancellationToken)
+//         {
+//             // if (!_workspaceService.TryGetFile(request.FileName, out ScriptFile scriptFile))
+//             // {
+//             //     throw new InvalidOperationException("This should not happen as PrepareRename should have already checked for viability. File an issue if you see this.");
+//             // }
+
+//             return await Task.Run(() =>
+//             {
+//                 ScriptFile scriptFile = _workspaceService.GetFile(new Uri(request.FileName));
+//                 Ast token = Utilities.GetAst(request.Line + 1, request.Column + 1, scriptFile.ScriptAst);
+//                 if (token == null) { return null; }
+
+//
+
+//                 return result;
+//             }).ConfigureAwait(false);
+//         }
+//     }
+// }
