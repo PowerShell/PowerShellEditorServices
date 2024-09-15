788ac731b418084c85b876c8d4807d86bcac7164
Justin Grote <JustinGrote@users.noreply.github.com>
2024-09-14 23:53:17 -0700

    Rework initial AST filter

---
+++ test/PowerShellEditorServices.Test/Refactoring/PrepareRenameHandlerTests.cs
@@ -0,0 +1,57 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+#nullable enable
+
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging.Abstractions;
+using Microsoft.PowerShell.EditorServices.Handlers;
+using Microsoft.PowerShell.EditorServices.Services;
+using Microsoft.PowerShell.EditorServices.Test.Shared;
+using OmniSharp.Extensions.LanguageServer.Protocol;
+using OmniSharp.Extensions.LanguageServer.Protocol.Models;
+using Xunit;
+using static PowerShellEditorServices.Test.Refactoring.RefactorFunctionTests;
+using static PowerShellEditorServices.Test.Refactoring.RefactorUtilities;
+
+namespace PowerShellEditorServices.Handlers.Test;
+
+[Trait("Category", "PrepareRename")]
+public class PrepareRenameHandlerTests : TheoryData<RenameSymbolParamsSerialized>
+{
+    private readonly WorkspaceService workspace = new(NullLoggerFactory.Instance);
+    private readonly PrepareRenameHandler handler;
+    public PrepareRenameHandlerTests()
+    {
+        workspace.WorkspaceFolders.Add(new WorkspaceFolder
+        {
+            Uri = DocumentUri.FromFileSystemPath(TestUtilities.GetSharedPath("Refactoring"))
+        });
+        handler = new(workspace);
+    }
+
+    // TODO: Test an untitled document (maybe that belongs in E2E)
+
+    [Theory]
+    [ClassData(typeof(FunctionRenameTestData))]
+    public async Task FindsSymbol(RenameSymbolParamsSerialized param)
+    {
+        // The test data is the PS script location. The handler expects 0-based line and column numbers.
+        Position position = new(param.Line - 1, param.Column - 1);
+        PrepareRenameParams testParams = new()
+        {
+            Position = position,
+            TextDocument = new TextDocumentIdentifier
+            {
+                Uri = DocumentUri.FromFileSystemPath(
+                    TestUtilities.GetSharedPath($"Refactoring/Functions/{param.FileName}")
+                )
+            }
+        };
+
+        RangeOrPlaceholderRange result = await handler.Handle(testParams, CancellationToken.None);
+        Assert.NotNull(result);
+        Assert.NotNull(result.Range);
+        Assert.True(result.Range.Contains(position));
+    }
+}
