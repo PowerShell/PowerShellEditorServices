3036b57df856269a1cfeb6d261337eda7f8359a7
Justin Grote <JustinGrote@users.noreply.github.com>
2024-09-15 13:36:28 -0700

    Rework RenameHandler to use Adapters

--- src/PowerShellEditorServices/Services/PowerShell/Handlers/RenameHandler.cs
+++ src/PowerShellEditorServices/Services/PowerShell/Handlers/RenameHandler.cs
@@ -13,6 +13,8 @@ using OmniSharp.Extensions.LanguageServer.Protocol.Document;
 using OmniSharp.Extensions.LanguageServer.Protocol.Models;
 using OmniSharp.Extensions.LanguageServer.Protocol.Client.Capabilities;
 using OmniSharp.Extensions.LanguageServer.Protocol;
+using System;
+using PowerShellEditorServices.Services.PowerShell.Utility;
 
 namespace Microsoft.PowerShell.EditorServices.Handlers;
 
@@ -34,14 +36,8 @@ internal class PrepareRenameHandler(WorkspaceService workspaceService) : IPrepar
             throw new HandlerErrorException("Dot Source detected, this is currently not supported");
         }
 
-        ScriptPosition scriptPosition = request.Position;
-        int line = scriptPosition.Line;
-        int column = scriptPosition.Column;
-
-        // FIXME: Refactor out to utility when working
-
-        Ast token = FindRenamableSymbol(scriptFile, line, column);
-
+        ScriptPositionAdapter position = request.Position;
+        Ast token = FindRenamableSymbol(scriptFile, position);
         if (token is null) { return null; }
 
         // TODO: Really should have a class with implicit convertors handing these conversions to avoid off-by-one mistakes.
@@ -49,17 +45,29 @@ internal class PrepareRenameHandler(WorkspaceService workspaceService) : IPrepar
     }
 
     /// <summary>
-    /// Finds a renamable symbol at a given position in a script file.
-    /// </summary>
+    /// Finds a renamable symbol at a given position in a script file using 1-based row/column references
     /// <param name="scriptFile"/>
     /// <param name="line">1-based line number</param>
     /// <param name="column">1-based column number</param>
+    /// </summary>
+    internal static Ast FindRenamableSymbol(ScriptFile scriptFile, int line, int column) =>
+        FindRenamableSymbol(scriptFile, new ScriptPositionAdapter(line, column));
+
+    /// <summary>
+    /// Finds a renamable symbol at a given position in a script file.
+    /// </summary>
     /// <returns>Ast of the token or null if no renamable symbol was found</returns>
-    internal static Ast FindRenamableSymbol(ScriptFile scriptFile, ScriptPosition position)
+    internal static Ast FindRenamableSymbol(ScriptFile scriptFile, ScriptPositionAdapter position)
     {
+        int line = position.Line;
+        int column = position.Column;
+
         // Cannot use generic here as our desired ASTs do not share a common parent
         Ast token = scriptFile.ScriptAst.Find(ast =>
         {
+            // Skip all statements that end before our target line or start after our target line. This is a performance optimization.
+            if (ast.Extent.EndLineNumber < line || ast.Extent.StartLineNumber > line) { return false; }
+
             // Supported types, filters out scriptblocks and whatnot
             if (ast is not (
                 FunctionDefinitionAst
@@ -73,9 +81,6 @@ internal class PrepareRenameHandler(WorkspaceService workspaceService) : IPrepar
                 return false;
             }
 
-            // Skip all statements that end before our target line or start after our target line. This is a performance optimization.
-            if (ast.Extent.EndLineNumber < line || ast.Extent.StartLineNumber > line) { return false; }
-
             // Special detection for Function calls that dont follow verb-noun syntax e.g. DoThing
             // It's not foolproof but should work in most cases where it is explicit (e.g. not & $x)
             if (ast is StringConstantExpressionAst stringAst)
@@ -84,13 +89,7 @@ internal class PrepareRenameHandler(WorkspaceService workspaceService) : IPrepar
                 if (parent.GetCommandName() != stringAst.Value) { return false; }
             }
 
-            Range astRange = new(
-                ast.Extent.StartLineNumber,
-                ast.Extent.StartColumnNumber,
-                ast.Extent.EndLineNumber,
-                ast.Extent.EndColumnNumber
-            );
-            return astRange.Contains(position);
+            return ast.Extent.Contains(position);
         }, true);
         return token;
     }
@@ -108,9 +107,9 @@ internal class RenameHandler(WorkspaceService workspaceService) : IRenameHandler
     public async Task<WorkspaceEdit> Handle(RenameParams request, CancellationToken cancellationToken)
     {
         ScriptFile scriptFile = workspaceService.GetFile(request.TextDocument.Uri);
-        ScriptPosition scriptPosition = request.Position;
+        ScriptPositionAdapter position = request.Position;
 
-        Ast tokenToRename = PrepareRenameHandler.FindRenamableSymbol(scriptFile, scriptPosition.Line, scriptPosition.Column);
+        Ast tokenToRename = PrepareRenameHandler.FindRenamableSymbol(scriptFile, position);
 
         // TODO: Potentially future cross-file support
         TextEdit[] changes = tokenToRename switch
@@ -193,24 +192,84 @@ public class RenameSymbolOptions
 }
 
 /// <summary>
-/// Represents a position in a script file that adapts and implicitly converts based on context. PowerShell script lines/columns start at 1, but LSP textdocument lines/columns start at 0. The default constructor is 1-based.
+/// Represents a position in a script file that adapts and implicitly converts based on context. PowerShell script lines/columns start at 1, but LSP textdocument lines/columns start at 0. The default line/column constructor is 1-based.
 /// </summary>
-internal record ScriptPosition(int Line, int Column)
+public record ScriptPositionAdapter(IScriptPosition position) : IScriptPosition, IComparable<ScriptPositionAdapter>, IComparable<Position>, IComparable<ScriptPosition>
 {
-    public static implicit operator ScriptPosition(Position position) => new(position.Line + 1, position.Character + 1);
-    public static implicit operator Position(ScriptPosition position) => new() { Line = position.Line - 1, Character = position.Column - 1 };
+    public int Line => position.LineNumber;
+    public int Column => position.ColumnNumber;
+    public int Character => position.ColumnNumber;
+    public int LineNumber => position.LineNumber;
+    public int ColumnNumber => position.ColumnNumber;
 
-    internal ScriptPosition Delta(int LineAdjust, int ColumnAdjust) => new(
-        Line + LineAdjust,
-        Column + ColumnAdjust
+    public string File => position.File;
+    string IScriptPosition.Line => position.Line;
+    public int Offset => position.Offset;
+
+    public ScriptPositionAdapter(int Line, int Column) : this(new ScriptPosition(null, Line, Column, null)) { }
+    public ScriptPositionAdapter(Position position) : this(position.Line + 1, position.Character + 1) { }
+
+    public static implicit operator ScriptPositionAdapter(Position position) => new(position);
+    public static implicit operator ScriptPositionAdapter(ScriptPosition position) => new(position);
+
+    public static implicit operator Position(ScriptPositionAdapter scriptPosition) => new(scriptPosition.position.LineNumber - 1, scriptPosition.position.ColumnNumber - 1);
+    public static implicit operator ScriptPosition(ScriptPositionAdapter position) => position;
+
+    internal ScriptPositionAdapter Delta(int LineAdjust, int ColumnAdjust) => new(
+        position.LineNumber + LineAdjust,
+        position.ColumnNumber + ColumnAdjust
     );
+
+    public int CompareTo(ScriptPositionAdapter other)
+    {
+        if (position.LineNumber == other.position.LineNumber)
+        {
+            return position.ColumnNumber.CompareTo(other.position.ColumnNumber);
+        }
+        return position.LineNumber.CompareTo(other.position.LineNumber);
+    }
+    public int CompareTo(Position other) => CompareTo((ScriptPositionAdapter)other);
+    public int CompareTo(ScriptPosition other) => CompareTo((ScriptPositionAdapter)other);
+    public string GetFullScript() => throw new NotImplementedException();
 }
 
-internal record ScriptRange(ScriptPosition Start, ScriptPosition End)
+/// <summary>
+/// Represents a range in a script file that adapts and implicitly converts based on context. PowerShell script lines/columns start at 1, but LSP textdocument lines/columns start at 0. The default ScriptExtent constructor is 1-based
+/// </summary>
+/// <param name="extent"></param>
+internal record ScriptExtentAdapter(IScriptExtent extent) : IScriptExtent
 {
-    // Positions will adjust per ScriptPosition
-    public static implicit operator ScriptRange(Range range) => new(range.Start, range.End);
-    public static implicit operator Range(ScriptRange range) => new() { Start = range.Start, End = range.End };
+    public readonly ScriptPositionAdapter Start = new(extent.StartScriptPosition);
+    public readonly ScriptPositionAdapter End = new(extent.StartScriptPosition);
+
+    public static implicit operator ScriptExtentAdapter(ScriptExtent extent) => new(extent);
+    public static implicit operator ScriptExtent(ScriptExtentAdapter extent) => extent;
+
+    public static implicit operator Range(ScriptExtentAdapter extent) => new()
+    {
+        // Will get shifted to 0-based
+        Start = extent.Start,
+        End = extent.End
+    };
+    public static implicit operator ScriptExtentAdapter(Range range) => new(new ScriptExtent(
+        // Will get shifted to 1-based
+        new ScriptPositionAdapter(range.Start),
+        new ScriptPositionAdapter(range.End)
+    ));
+
+    public IScriptPosition StartScriptPosition => Start;
+    public IScriptPosition EndScriptPosition => End;
+    public int EndColumnNumber => End.ColumnNumber;
+    public int EndLineNumber => End.LineNumber;
+    public int StartOffset => extent.EndOffset;
+    public int EndOffset => extent.EndOffset;
+    public string File => extent.File;
+    public int StartColumnNumber => extent.StartColumnNumber;
+    public int StartLineNumber => extent.StartLineNumber;
+    public string Text => extent.Text;
+
+    public bool Contains(Position position) => ContainsPosition(this, position);
+    public static bool ContainsPosition(ScriptExtentAdapter range, ScriptPositionAdapter position) => Range.ContainsPosition(range, position);
 }
 
 public class RenameSymbolParams : IRequest<RenameSymbolResult>
