e6d39337a10dd4979069847666febe552074b5ad
Justin Grote <JustinGrote@users.noreply.github.com>
2024-09-17 16:57:25 -0700

    Redo tests to be data-driven

--- test/PowerShellEditorServices.Test.Shared/Refactoring/Utilities/RefactorUtilitiesData.cs
+++ test/PowerShellEditorServices.Test.Shared/Refactoring/Utilities/RefactorUtilitiesData.cs
@@ -1,59 +1,57 @@
 ﻿// Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
-using Microsoft.PowerShell.EditorServices.Handlers;
 
-namespace PowerShellEditorServices.Test.Shared.Refactoring.Utilities
+namespace PowerShellEditorServices.Test.Shared.Refactoring
 {
     internal static class RenameUtilitiesData
     {
-
-        public static readonly RenameSymbolParams GetVariableExpressionAst = new()
+        public static readonly RenameTestTarget GetVariableExpressionAst = new()
         {
             Column = 11,
             Line = 15,
-            RenameTo = "Renamed",
+            NewName = "Renamed",
             FileName = "TestDetection.ps1"
         };
-        public static readonly RenameSymbolParams GetVariableExpressionStartAst = new()
+        public static readonly RenameTestTarget GetVariableExpressionStartAst = new()
         {
             Column = 1,
             Line = 15,
-            RenameTo = "Renamed",
+            NewName = "Renamed",
             FileName = "TestDetection.ps1"
         };
-        public static readonly RenameSymbolParams GetVariableWithinParameterAst = new()
+        public static readonly RenameTestTarget GetVariableWithinParameterAst = new()
         {
             Column = 21,
             Line = 3,
-            RenameTo = "Renamed",
+            NewName = "Renamed",
             FileName = "TestDetection.ps1"
         };
-        public static readonly RenameSymbolParams GetHashTableKey = new()
+        public static readonly RenameTestTarget GetHashTableKey = new()
         {
             Column = 9,
             Line = 16,
-            RenameTo = "Renamed",
+            NewName = "Renamed",
             FileName = "TestDetection.ps1"
         };
-        public static readonly RenameSymbolParams GetVariableWithinCommandAst = new()
+        public static readonly RenameTestTarget GetVariableWithinCommandAst = new()
         {
             Column = 29,
             Line = 6,
-            RenameTo = "Renamed",
+            NewName = "Renamed",
             FileName = "TestDetection.ps1"
         };
-        public static readonly RenameSymbolParams GetCommandParameterAst = new()
+        public static readonly RenameTestTarget GetCommandParameterAst = new()
         {
             Column = 12,
             Line = 21,
-            RenameTo = "Renamed",
+            NewName = "Renamed",
             FileName = "TestDetection.ps1"
         };
-        public static readonly RenameSymbolParams GetFunctionDefinitionAst = new()
+        public static readonly RenameTestTarget GetFunctionDefinitionAst = new()
         {
             Column = 12,
             Line = 1,
-            RenameTo = "Renamed",
+            NewName = "Renamed",
             FileName = "TestDetection.ps1"
         };
     }
