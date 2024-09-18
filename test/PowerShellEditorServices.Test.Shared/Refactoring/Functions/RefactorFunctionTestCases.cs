769504bfdefe56710eebcb11ff3d1706e69ac815
Justin Grote <JustinGrote@users.noreply.github.com>
2024-09-17 23:22:22 -0700

    Fixed all function Prepare tests

--- test/PowerShellEditorServices.Test.Shared/Refactoring/Functions/RefactorFunctionTestCases.cs
+++ test/PowerShellEditorServices.Test.Shared/Refactoring/Functions/RefactorFunctionTestCases.cs
@@ -5,21 +5,24 @@ namespace PowerShellEditorServices.Test.Shared.Refactoring;
 
 public class RefactorFunctionTestCases
 {
+    /// <summary>
+    /// Defines where functions should be renamed. These numbers are 1-based.
+    /// </summary>
     public static RenameTestTarget[] TestCases =
     [
-        new("FunctionsSingle.ps1",                     Line: 1,  Column: 11 ),
-        new("FunctionMultipleOccurrences.ps1",         Line: 1,  Column:  5 ),
-        new("FunctionInnerIsNested.ps1",               Line: 5,  Column:  5, "bar"),
-        new("FunctionOuterHasNestedFunction.ps1",      Line: 10, Column:  1 ),
-        new("FunctionWithInnerFunction.ps1",           Line: 5,  Column:  5, "RenamedInnerFunction"),
-        new("FunctionWithInternalCalls.ps1",           Line: 1,  Column:  5 ),
-        new("FunctionCmdlet.ps1",                      Line: 10, Column:  1 ),
-        new("FunctionSameName.ps1",                    Line: 14, Column:  3, "RenamedSameNameFunction"),
-        new("FunctionScriptblock.ps1",                 Line: 5,  Column:  5 ),
-        new("FunctionLoop.ps1",                        Line: 5,  Column:  5 ),
-        new("FunctionForeach.ps1",                     Line: 5,  Column: 11 ),
-        new("FunctionForeachObject.ps1",               Line: 5,  Column: 11 ),
-        new("FunctionCallWIthinStringExpression.ps1",  Line: 1,  Column: 10 ),
-        new("FunctionNestedRedefinition.ps1",          Line: 13, Column: 15 )
+        new("FunctionCallWIthinStringExpression.ps1",  Line:  1, Column: 10 ),
+        new("FunctionCmdlet.ps1",                      Line:  1, Column: 10 ),
+        new("FunctionForeach.ps1",                     Line:  5, Column: 11 ),
+        new("FunctionForeachObject.ps1",               Line:  5, Column: 11 ),
+        new("FunctionInnerIsNested.ps1",               Line:  5, Column:  5  , "bar"),
+        new("FunctionLoop.ps1",                        Line:  5, Column:  5 ),
+        new("FunctionMultipleOccurrences.ps1",         Line:  5, Column:  3 ),
+        new("FunctionNestedRedefinition.ps1",          Line: 13, Column: 15 ),
+        new("FunctionOuterHasNestedFunction.ps1",      Line:  2, Column: 15 ),
+        new("FunctionSameName.ps1",                    Line:  3, Column:  14 , "RenamedSameNameFunction"),
+        new("FunctionScriptblock.ps1",                 Line:  5, Column:  5 ),
+        new("FunctionsSingle.ps1",                     Line:  1, Column: 11 ),
+        new("FunctionWithInnerFunction.ps1",           Line:  5, Column:  5  , "RenamedInnerFunction"),
+        new("FunctionWithInternalCalls.ps1",           Line:  3, Column:  6 ),
     ];
 }
