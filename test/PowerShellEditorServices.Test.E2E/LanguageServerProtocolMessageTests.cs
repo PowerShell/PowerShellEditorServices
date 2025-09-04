525188c0cb3f1c9945431db9086045e3df1cc42f
Andy Jordan <2226434+andyleejordan@users.noreply.github.com>
2025-09-04 16:33:57 -0700

    Increase test timeout to workaround overloaded OneBranch runners
    Seriously.

--- test/PowerShellEditorServices.Test.E2E/LanguageServerProtocolMessageTests.cs
+++ test/PowerShellEditorServices.Test.E2E/LanguageServerProtocolMessageTests.cs
@@ -83,9 +83,9 @@ namespace PowerShellEditorServices.Test.E2E
             // Wait for PSSA to finish.
             for (int i = 0; Diagnostics.Count == 0; i++)
             {
-                if (i >= 30)
+                if (i >= 120)
                 {
-                    throw new InvalidDataException("No diagnostics showed up after 20s.");
+                    throw new InvalidDataException("No diagnostics showed up after 2 minutes.");
                 }
 
                 await Task.Delay(1000);
